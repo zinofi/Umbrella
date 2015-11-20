08309b42cb4f587ee961caa4f2bfa89ff4c026b3
Richard Edwards <richard.edwards@zinofi.co.uk>
2015-11-20 00:09:41 +0000

    Upgraded to rc1 Added some additional files

---
+++ DNX/Umbrella.DNX.WebUtilities/Mvc/UmbrellaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using log4net;
+using System.Runtime.CompilerServices;
+using Umbrella.Utilities.Extensions;
+
+namespace Umbrella.DNX.WebUtilities.Mvc
+{
+    public class UmbrellaController : Controller
+    {
+        #region Protected Static Members
+        protected static readonly ILog Log = LogManager.GetLogger(typeof(UmbrellaController));
+        #endregion
+
+        #region Protected Methods
+        protected bool LogError(Exception exc, object model = null, string message = "", bool returnValue = false, [CallerMemberName]string methodName = "") => Log.LogError(exc, model, message, returnValue, methodName);
+        #endregion
+    }
+}
---
+++ General/Umbrella.Utilities/Extensions/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Umbrella.Utilities.Extensions
+{
+    public static class ObjectExtensions
+    {
+        #region Private Static Members
+        private static readonly JsonSerializerSettings s_CamelCaseJsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+        #endregion
+
+        #region Public Static Methods
+        public static string ToJsonString(this object value, bool useCamelCasingRules = false)
+        {
+            return useCamelCasingRules
+                ? JsonConvert.SerializeObject(value)
+                : JsonConvert.SerializeObject(value, s_CamelCaseJsonSerializerSettings);
+        }
+        #endregion
+    }
+}
