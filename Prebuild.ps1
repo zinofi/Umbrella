1103ab948a656d490b439fe9b649c2fd1fae6f64
Richard Edwards <richard.edwards@zinofi.co.uk>
2015-11-20 00:20:59 +0000

    Updated Prebuild powershell script

--- Prebuild.ps1
+++ Prebuild.ps1
@@ -20,5 +20,5 @@ else
 # ex: & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
- # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
+ # run DNU restore on all project.json files in the folder structure including 2>1 to redirect stderr to stdout for badly behaved tools
+Get-ChildItem -Path $PSScriptRoot -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
