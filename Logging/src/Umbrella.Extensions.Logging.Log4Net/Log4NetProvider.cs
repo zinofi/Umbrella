e1a3f5abc8766be9690f50d2e67d031e42d25a88
Richard Edwards <richard.edwards@zinofi.co.uk>
2016-02-12 21:18:35 +0000

    Fixes Updated to build00015

--- Logging/src/Umbrella.Extensions.Logging.Log4Net/Log4NetProvider.cs
+++ Logging/src/Umbrella.Extensions.Logging.Log4Net/Log4NetProvider.cs
@@ -1,32 +1,22 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace Umbrella.Extensions.Logging.Log4Net
 {
     public class Log4NetProvider : ILoggerProvider
     {
-        private IDictionary<string, ILogger> _loggers = new Dictionary<string, ILogger>();
+        private ConcurrentDictionary<string, ILogger> m_LoggersDictionary = new ConcurrentDictionary<string, ILogger>();
 
         public ILogger CreateLogger(string name)
         {
-            if (!_loggers.ContainsKey(name))
-            {
-                lock (_loggers)
-                {
-                    // Have to check again since another thread may have gotten the lock first
-                    if (!_loggers.ContainsKey(name))
-                    {
-                        _loggers[name] = new Log4NetAdapter(name);
-                    }
-                }
-            }
-            return _loggers[name];
+            return m_LoggersDictionary.GetOrAdd(name, (x) => new Log4NetAdapter(x));
         }
 
         public void Dispose()
         {
-            _loggers.Clear();
-            _loggers = null;
+            m_LoggersDictionary.Clear();
+            m_LoggersDictionary = null;
         }
     }
 }
