f671628a57248c323374a5770df663b691430623
Richard Edwards <richard.edwards@zinofi.co.uk>
2016-05-18 03:41:37 +0100

    More powershell changes

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,5 @@
  # run dotnet restore which will restore all packages by examing the global.json file and finding the project.json files automatically
-& $env:USERPROFILE\Microsoft\DotNet\dotnet restore
+& $env:LOCALAPPDATA\Microsoft\DotNet\dotnet restore
 
 # run dotnet build on all project.json files in the folder structure
-& $env:USERPROFILE\Microsoft\DotNet\dotnet build $PSScriptRoot/**/project.json
+& $env:LOCALAPPDATA\Microsoft\DotNet\dotnet build $PSScriptRoot/**/project.json
