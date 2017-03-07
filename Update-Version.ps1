55dde00e984ac84316dba89dc0dd66c8a320c5f0
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-03-07 23:39:17 +0000

    Added a powershell script to allow the package version to be updated inside all .csproj files. Updated version to build00095

---
+++ Update-Version.ps1
@@ -0,0 +1,7 @@
+﻿$configFiles = Get-ChildItem . *.csproj -rec
+foreach ($file in $configFiles)
+{
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "build00094", "build00095" } |
+    Set-Content $file.PSPath
+}
