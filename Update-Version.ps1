f7d414d8c77d09684b7de27f79deb099e5093a58
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-04-23 02:34:44 +0100

    Updated version to build00322

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,6 +2,6 @@
 foreach ($file in $configFiles)
 {
     (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "build00320", "build00321" } |
+    Foreach-Object { $_ -replace "build00321", "build00322" } |
     Set-Content $file.PSPath
 }
