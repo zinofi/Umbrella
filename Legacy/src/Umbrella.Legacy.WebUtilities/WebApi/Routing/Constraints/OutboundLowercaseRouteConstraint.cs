3ef0b1820eaaef8d6ad5087e5adc44ebd4529055
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-05-08 18:18:12 +0100

    Updated Moq to latest version Added a new route constraint to the legacy web utilities

---
+++ Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/Routing/Constraints/OutboundLowercaseRouteConstraint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace Umbrella.Legacy.WebUtilities.WebApi.Routing.Constraints
+{
+    public class OutboundLowercaseRouteConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (routeDirection == RouteDirection.IncomingRequest)
+                return true;
+
+            var path = httpContext.Request.Url.AbsolutePath;
+
+            return path.Equals(path.ToLowerInvariant(), StringComparison.InvariantCulture);
+        }
+    }
+}
