8eadab8038a008f880462ec851bfcf5368d83c73
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-05-12 19:53:09 +0100

    Added classes to help solve bundle ordering and url rewrite issues for css files.

---
+++ Legacy/src/Umbrella.Legacy.WebUtilities/Bundling/AsIsBundleOrderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace Umbrella.Legacy.WebUtilities.Bundling
+{
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+}
---
+++ Legacy/src/Umbrella.Legacy.WebUtilities/Bundling/CssRewriteUrlVirtualPathTransform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Umbrella.Legacy.WebUtilities.Bundling
+{
+    public class CssRewriteUrlVirtualPathTransform : IItemTransform
+    {
+        public string Process(string includedVirtualPath, string input)
+        {
+            return new CssRewriteUrlTransform().Process("~" + VirtualPathUtility.ToAbsolute(includedVirtualPath), input);
+        }
+    }
+}
