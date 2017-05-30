8fcade7ec3216836dba4ac035e5ef21da57457b4
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-05-31 00:26:56 +0100

    Updated version to build00349

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,6 +2,6 @@
 foreach ($file in $configFiles)
 {
     (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "build00347", "build00348" } |
+    Foreach-Object { $_ -replace "build00348", "build00349" } |
     Set-Content $file.PSPath
 }
