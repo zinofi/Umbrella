d5dab9b72ed274f7ebf818fdf903a3c0be7c32ae
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-07-12 17:58:40 +0100

    Updated version to build00360

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -1,7 +1,26 @@
 ﻿$configFiles = Get-ChildItem . *.csproj -rec
+$affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
+
+$previousBuild = "build00359"
+$currentBuild = "build00360"
+
 foreach ($file in $configFiles)
 {
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "build00355", "build00356" } |
-    Set-Content $file.PSPath
+	$content = Get-Content $file.PSPath
+
+	if($content -like "*" + $previousBuild + "*")
+	{
+		$affectedFiles.Add($file.Name)
+		$content -replace $previousBuild, $currentBuild | Set-Content $file.PSPath
+	}
+}
+
+Write-Output "Total files: " $configFiles.Length
+Write-Output "Total affected files: " $affectedFiles.Count
+
+if(!$affectedFiles.Count.Equals(0))
+{
+	Write-Output ""
+	Write-Output "Affected Files: "
+	$affectedFiles | ForEach-Object { Write-Output $_ }
 }
