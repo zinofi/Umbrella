d40d7eb0747bc24ed16e6ad6f281d3c93507e9a4
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-08-11 21:07:55 +0100

    Added NameValueCollection extensions methods

---
+++ Core/src/Umbrella.Utilities/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿#if NET46
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbrella.Utilities.Extensions
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static string SerializeToString(this NameValueCollection nvc, string keyValueSeparator = "=", string pairSeparator = "&")
+            => SerializeToString(nvc, WebUtility.UrlEncode, keyValueSeparator, pairSeparator);
+
+        public static string SerializeToString(this NameValueCollection nvc, Func<string, string> urlEncoder, string keyValueSeparator = "=", string pairSeparator = "&")
+        {
+            Guard.ArgumentNotNull(nvc, nameof(nvc));
+            Guard.ArgumentNotNull(urlEncoder, nameof(urlEncoder));
+            Guard.ArgumentNotNullOrWhiteSpace(keyValueSeparator, nameof(keyValueSeparator));
+            Guard.ArgumentNotNullOrWhiteSpace(pairSeparator, nameof(pairSeparator));
+
+            return string.Join(pairSeparator,
+                        nvc.AllKeys.Where(key => !string.IsNullOrWhiteSpace(nvc[key]))
+                            .Select(
+                                key => string.Join(pairSeparator, nvc.GetValues(key).Select(val => $"{urlEncoder(key)}{keyValueSeparator}{urlEncoder(val)}"))));
+        }
+    }
+}
+#endif
