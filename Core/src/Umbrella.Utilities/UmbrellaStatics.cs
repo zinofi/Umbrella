539b9d5f9c8c46dd772e6f622e765102289d4871
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-10-05 04:10:18 +0100

    Fixes

--- Core/src/Umbrella.Utilities/UmbrellaStatics.cs
+++ Core/src/Umbrella.Utilities/UmbrellaStatics.cs
@@ -5,14 +5,15 @@ namespace Umbrella.Utilities
     public static class UmbrellaStatics
     {
         public delegate string SerializeJsonDelegate(object value, bool useCamelCasingRules);
-        public delegate object DeserializeJsonDelegate(string value);
+        public delegate object DeserializeJsonDelegate(string value, Type type);
 
         public static SerializeJsonDelegate JsonSerializer { private get; set; }
-
         public static DeserializeJsonDelegate JsonDeserializer { private get; set; }
 
         public static string SerializeJson(object value, bool useCamelCasingRules = false)
         {
+            Guard.ArgumentNotNull(value, nameof(value));
+
             if(JsonSerializer == null)
                 throw new Exception("The JsonSerializer has not been assigned. This should be done on application startup.");
 
@@ -21,10 +22,12 @@ namespace Umbrella.Utilities
 
         public static T DeserializeJson<T>(string value)
         {
+            Guard.ArgumentNotNullOrWhiteSpace(value, nameof(value));
+
             if(JsonDeserializer == null)
                 throw new Exception("The JsonDeserializer has not been assigned. This should be done on application startup.");
 
-            return (T)JsonDeserializer(value);
+            return (T)JsonDeserializer(value, typeof(T));
         }
     }
 }
