65f58e657237bdb4fff893f00f57ec4626517215
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-11-12 22:58:29 +0000

    Updated version to build00377

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -1,8 +1,8 @@
 ﻿$configFiles = Get-ChildItem . *.csproj -rec
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
-$previousBuild = "1.0.0-rel-build00375"
-$currentBuild = "1.0.0-rel-build00376"
+$previousBuild = "1.0.0-rel-build00376"
+$currentBuild = "1.0.0-rel-build00377"
 
 foreach ($file in $configFiles)
 {
