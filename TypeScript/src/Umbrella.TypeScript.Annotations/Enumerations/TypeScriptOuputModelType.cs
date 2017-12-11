e224293759cd76c90c6906aedc869c53cc6986c9
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-12-11 19:52:14 +0000

    Altered the ouput model type enum flags to use binary instead of hexadecimal literals.

--- TypeScript/src/Umbrella.TypeScript.Annotations/Enumerations/TypeScriptOuputModelType.cs
+++ TypeScript/src/Umbrella.TypeScript.Annotations/Enumerations/TypeScriptOuputModelType.cs
@@ -5,11 +5,11 @@ namespace Umbrella.TypeScript
     [Flags]
     public enum TypeScriptOutputModelType
     {
-        Interface =         0x00000001,
-        Class =             0x00000011, //Ensure both interface and class will be generated
-        KnockoutInterface = 0x00000100,
-        KnockoutClass =     0x00001100, //Ensure both interface and class will be generated
-        AureliaInterface =  0x00010000,
-        AureliaClass =      0x00110000  //Ensure both interface and class will be generated
+        Interface =         0b00000001,
+        Class =             0b00000011, //Ensure both interface and class will be generated
+        KnockoutInterface = 0b00000100,
+        KnockoutClass =     0b00001100, //Ensure both interface and class will be generated
+        AureliaInterface =  0b00010000,
+        AureliaClass =      0b00110000  //Ensure both interface and class will be generated
     }
 }
