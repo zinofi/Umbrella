bf0291e760d5d20516bd8acd5aa9112560b74b79
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-12-12 02:20:26 +0000

    Updated version to 2.0.0-rc-00003

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -1,8 +1,8 @@
 ﻿$configFiles = Get-ChildItem . *.csproj -rec
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
-$previousBuild = "2.0.0-rc-00001"
-$currentBuild = "2.0.0-rc-00002"
+$previousBuild = "2.0.0-rc-00002"
+$currentBuild = "2.0.0-rc-00003"
 
 foreach ($file in $configFiles)
 {
