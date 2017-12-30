917a9c3ee1e53745416b01fe59bca81efd3dfa4f
Richard Edwards <richard.edwards@zinofi.co.uk>
2017-12-30 02:13:06 +0000

    Replaced protected methods with new version which return plain text strings instead of strings wrapped inside a JSON string.

--- Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/UmbrellaApiController.cs
+++ Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/UmbrellaApiController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Results;
 using Umbrella.Legacy.WebUtilities.WebApi.Filters;
@@ -24,13 +26,21 @@ namespace Umbrella.Legacy.WebUtilities.WebApi
         #endregion
 
         #region Protected Methods
-        protected NegotiatedContentResult<string> NotFound(string reason) => Content(HttpStatusCode.NotFound, reason);
-        protected NegotiatedContentResult<string> Conflict(string reason) => Content(HttpStatusCode.Conflict, reason);
-        protected NegotiatedContentResult<string> InternalServerError(string reason) => Content(HttpStatusCode.InternalServerError, reason);
-        protected NegotiatedContentResult<string> Unauthorized(string reason) => Content(HttpStatusCode.Unauthorized, reason);
-        protected NegotiatedContentResult<string> Forbidden(string reason) => Content(HttpStatusCode.Forbidden, reason);
-        protected NegotiatedContentResult<string> MethodNotAllowed(string reason) => Content(HttpStatusCode.MethodNotAllowed, reason);
+        protected ResponseMessageResult NotFound(string reason) => CreateStringContentResult(HttpStatusCode.NotFound, reason);
+        protected ResponseMessageResult Conflict(string reason) => CreateStringContentResult(HttpStatusCode.Conflict, reason);
+        protected ResponseMessageResult InternalServerError(string reason) => CreateStringContentResult(HttpStatusCode.InternalServerError, reason);
+        protected ResponseMessageResult Unauthorized(string reason) => CreateStringContentResult(HttpStatusCode.Unauthorized, reason);
+        protected ResponseMessageResult Forbidden(string reason) => CreateStringContentResult(HttpStatusCode.Forbidden, reason);
+        protected ResponseMessageResult MethodNotAllowed(string reason) => CreateStringContentResult(HttpStatusCode.MethodNotAllowed, reason);
         protected StatusCodeResult NoContent() => new StatusCodeResult(HttpStatusCode.NoContent, this);
+        protected ResponseMessageResult TooManyRequests(string reason) => CreateStringContentResult((HttpStatusCode)429, reason);
+        protected ResponseMessageResult CreateStringContentResult(HttpStatusCode statusCode, string reason)
+        {
+            return new ResponseMessageResult(new HttpResponseMessage(statusCode)
+            {
+                Content = new StringContent(reason, Encoding.UTF8, "text/plain")
+            });
+        }
         #endregion
     }
 }
