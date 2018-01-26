34128e6ad493b2b1d2e7eff1d9a0f7902a99c22a
Richard Edwards <richard.edwards@zinofi.co.uk>
2018-01-26 07:06:25 +0000

    Added a new HashSetExtensions class.

---
+++ Core/src/Umbrella.Utilities/Extensions/HashSetExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbrella.Utilities.Extensions
+{
+    public static class HashSetExtensions
+    {
+        public static bool AddNotNull<T>(this HashSet<T> hashSet, T value)
+        {
+            if (value != null)
+                return hashSet.Add(value);
+
+            return false;
+        }
+
+        public static bool AddNotNullTrim(this HashSet<string> hashSet, string value)
+            => AddNotNull(hashSet, value?.Trim());
+    }
+}
