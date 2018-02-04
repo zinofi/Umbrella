d6e90a8e769d86cd65a2726a7837052191e192b1
Richard Edwards <richard.edwards@zinofi.co.uk>
2018-02-04 22:18:51 +0000

    Marked projects which should not be packaged with the IsPackable project property. Updated version to 2.0.0-rc-00008

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -1,8 +1,8 @@
 ﻿$configFiles = Get-ChildItem . *.csproj -rec
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
-$previousBuild = "2.0.0-rc-00006"
-$currentBuild =  "2.0.0-rc-00007"
+$previousBuild = "2.0.0-rc-00007"
+$currentBuild =  "2.0.0-rc-00008"
 
 foreach ($file in $configFiles)
 {
