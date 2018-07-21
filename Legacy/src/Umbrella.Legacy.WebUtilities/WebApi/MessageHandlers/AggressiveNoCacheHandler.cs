72c4852ab766433c5a53988e4138c323ffec981f
Richard Edwards <richard.edwards@zinofi.co.uk>
2018-07-21 01:49:12 +0100

    Updated the cache handler to add additional headers and only apply any headers when the response is a success code.

--- Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/MessageHandlers/AggressiveNoCacheHandler.cs
+++ Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/MessageHandlers/AggressiveNoCacheHandler.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace Umbrella.Legacy.WebUtilities.WebApi.MessageHandlers
 {
+    /// <summary>
+    /// A WebAPI delegating message handler to ensure responses are not cached by clients.
+    /// </summary>
     public class AggressiveNoCacheHandler : DelegatingHandler
     {
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -11,7 +14,14 @@ namespace Umbrella.Legacy.WebUtilities.WebApi.MessageHandlers
             HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
 
             if (response.IsSuccessStatusCode)
-                response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+            {
+                // Set standard HTTP/1.0 no-cache header (no-store, no-cache, must-revalidate)
+                // Set IE extended HTTP/1.1 no-cache headers (post-check, pre-check)
+                response.Headers.Add("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+
+                // Set standard HTTP/1.0 no-cache header.
+                response.Headers.Add("Pragma", "no-cache");
+            }
 
             return response;
         }
