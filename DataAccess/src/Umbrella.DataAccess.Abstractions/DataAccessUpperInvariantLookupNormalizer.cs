f6fd0ff792e8b63686d7ec5712655d740b1f799e
Richard Edwards <richard.edwards@zinofi.co.uk>
2018-08-07 23:42:55 +0100

    Added logging and took advantage of null propagation for cleaner code.

--- DataAccess/src/Umbrella.DataAccess.Abstractions/DataAccessUpperInvariantLookupNormalizer.cs
+++ DataAccess/src/Umbrella.DataAccess.Abstractions/DataAccessUpperInvariantLookupNormalizer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,12 +9,27 @@ namespace Umbrella.DataAccess.Abstractions
 {
     public class DataAccessUpperInvariantLookupNormalizer : IDataAccessLookupNormalizer
     {
+        #region Private Members
+        private readonly ILogger m_Log;
+        #endregion
+
+        #region Constructors
+        public DataAccessUpperInvariantLookupNormalizer(ILogger<DataAccessUpperInvariantLookupNormalizer> logger)
+            => m_Log = logger;
+        #endregion
+
+        #region IDataAccessLookupNormalizer Members
         public string Normalize(string value)
         {
-            if (value == null)
-                return null;
-
-            return value.Normalize().ToUpperInvariant();
-        }
+            try
+            {
+                return value?.Normalize()?.ToUpperInvariant();
+            }
+            catch (Exception exc) when (m_Log.WriteError(exc, message: "String normalization to form C failed. The source string has not been logged for security reasons."))
+            {
+                throw;
+            }
+        } 
+        #endregion
     }
 }
