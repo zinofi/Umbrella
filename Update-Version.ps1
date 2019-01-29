35ee86f7f0ba20598eaf42ece915a512fc396f8a
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-01-29 11:01:47 +0000

    Updated version to 2.1.0-rc-00019

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "2.1.0-rc-00017"
-$currentBuild =  "2.1.0-rc-00018"
+$previousBuild = "2.1.0-rc-00018"
+$currentBuild =  "2.1.0-rc-00019"
 
 foreach ($file in $configFiles)
 {
