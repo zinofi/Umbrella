65dd656cdc74e3dafca3199348e9f70d4bc3b882
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-03-04 15:05:35 +0000

    Updated version to 2.2.0

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "2.1.0"
-$currentBuild =  "2.1.1"
+$previousBuild = "2.1.1"
+$currentBuild =  "2.2.0"
 
 foreach ($file in $configFiles)
 {
