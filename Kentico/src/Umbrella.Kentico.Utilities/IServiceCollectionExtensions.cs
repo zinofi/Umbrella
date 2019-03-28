345322d7a93d54aac7a7ccd8d63b7c6e6b0c6a08
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-03-28 22:15:53 +0000

    Added the MergeMarketingContactMiddleware.

--- Kentico/src/Umbrella.Kentico.Utilities/IServiceCollectionExtensions.cs
+++ Kentico/src/Umbrella.Kentico.Utilities/IServiceCollectionExtensions.cs
@@ -7,6 +7,9 @@ using Umbrella.Kentico.Utilities.Users.Abstractions;
 using Umbrella.Kentico.Utilities.Users;
 using Umbrella.Kentico.Utilities.ContactManagement.Options;
 using System;
+using Umbrella.Kentico.Utilities.Middleware.Options;
+using Umbrella.Kentico.Utilities.Middleware;
+using Kentico.Activities;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -14,21 +17,30 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddUmbrellaKenticoUtilities(this IServiceCollection services)
 		{
+			// Contact Management
 			services.AddScoped(ctx => Service.Entry<IContactProcessingChecker>());
 			services.AddScoped(ctx => Service.Entry<IContactCreator>());
 			services.AddScoped(ctx => Service.Entry<IContactRelationAssigner>());
 			services.AddScoped(ctx => Service.Entry<IContactPersistentStorage>());
 			services.AddScoped(ctx => Service.Entry<IContactMergeService>());
+			services.AddSingleton<IMembershipActivitiesLogger, MembershipActivitiesLogger>();
+
 			services.AddScoped<IKenticoContactManager, KenticoContactManager>();
 			services.AddSingleton<IKenticoUserNameNormalizer, KenticoUserNameNormalizer>();
 
 			// Default Options - These can be replaced by calls to the Configure* methods below.
 			services.AddSingleton<KenticoContactManagerOptions>();
 
+			// Middleware
+			services.AddSingleton<MergeMarketingContactMiddleware>();
+
 			return services;
 		}
 
 		public static IServiceCollection ConfigureKenticoContactManagerOptions(this IServiceCollection services, Action<IServiceProvider, KenticoContactManagerOptions> optionsBuilder)
+			=> services.ConfigureUmbrellaOptions(optionsBuilder);
+
+		public static IServiceCollection ConfigureMergeMarketingContactMiddlewareOptions(this IServiceCollection services, Action<IServiceProvider, MergeMarketingContactMiddlewareOptions> optionsBuilder)
 			=> services.ConfigureUmbrellaOptions(optionsBuilder);
     }
 }
---
+++ Kentico/src/Umbrella.Kentico.Utilities/Middleware/Options/MergeMarketingContactMiddlewareOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Umbrella.Kentico.Utilities.Middleware.Options
+{
+    public class MergeMarketingContactMiddlewareOptions
+    {
+		public string KenticoSiteName { get; set; }
+		public string IsSigningInClaimType { get; set; }
+		public bool CookieSecure { get; set; }
+		public string CookieDomain { get; set; }
+		public TimeSpan CookieExpiration { get; set; }
+		public bool LogLoginActivity { get; set; }
+		public Func<string, bool> ShouldExecuteForPathDeterminer { get; set; }
+	}
+}
