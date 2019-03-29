7cc9313baa617a84084d4042437d320c7396655a
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-03-29 22:00:01 +0000

    Added IEnumerableValidationResultExtensions

---
+++ Legacy/src/Umbrella.Legacy.WebUtilities/WebApi/Extensions/IEnumerableValidationResultExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Http.ModelBinding;
+
+namespace Umbrella.Legacy.WebUtilities.WebApi.Extensions
+{
+	public static class IEnumerableValidationResultExtensions
+	{
+		public static ModelStateDictionary ToModelStateDictionary(this IEnumerable<ValidationResult> validationResults)
+		{
+			var dictionary = new ModelStateDictionary();
+
+			foreach (var item in validationResults)
+			{
+				var memberNames = item.MemberNames?.ToList();
+
+				if (memberNames?.Count > 0)
+				{
+					foreach (string key in item.MemberNames)
+					{
+						dictionary.AddModelError(key, item.ErrorMessage);
+					}
+				}
+				else
+				{
+					dictionary.AddModelError(string.Empty, item.ErrorMessage);
+				}
+			}
+
+			return dictionary;
+		}
+	}
+}
