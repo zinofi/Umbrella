dfef37b554d90d566a14d244fd221b4802e9f951
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-04-23 20:10:02 +0100

    Updated version to 2.6.0-preview-00019

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "2.6.0-preview-00017"
-$currentBuild =  "2.6.0-preview-00018"
+$previousBuild = "2.6.0-preview-00018"
+$currentBuild =  "2.6.0-preview-00019"
 
 foreach ($file in $configFiles)
 {
