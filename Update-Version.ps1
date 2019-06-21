0f3e4c24ed7628bcdff0794c165fcb94175f3e24
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-06-21 11:39:48 +0100

    Updated version to 2.8.0

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "2.7.0"
-$currentBuild =  "2.8.0-preview-001"
+$previousBuild = "2.8.0-preview-001"
+$currentBuild =  "2.8.0"
 
 foreach ($file in $configFiles)
 {
