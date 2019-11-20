52a5525a85ed41a8aac42b13b40feb6d4260515c
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-11-20 18:22:53 +0000

    XML documentation.

--- DataAccess/src/Umbrella.DataAccess.EF6/IQueryableExtensions.cs
+++ DataAccess/src/Umbrella.DataAccess.EF6/IQueryableExtensions.cs
@@ -4,8 +4,18 @@ using Umbrella.DataAccess.EF.Abstractions;
 
 namespace Umbrella.DataAccess.EF6
 {
+	/// <summary>
+	/// Extensions for the <see cref="IQueryable{T}"/> interface.
+	/// </summary>
 	public static class IQueryableExtensions
 	{
+		/// <summary>
+		/// Add the specified include map to the query.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="items">The items.</param>
+		/// <param name="map">The include map.</param>
+		/// <returns>The query.</returns>
 		public static IQueryable<TEntity> IncludeMap<TEntity>(this IQueryable<TEntity> items, IncludeMap<TEntity> map)
 			where TEntity : class
 		{
@@ -22,6 +32,13 @@ namespace Umbrella.DataAccess.EF6
 			return query;
 		}
 
+		/// <summary>
+		/// Specifies whether or not to create tracking entries for the current query.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="items">The items.</param>
+		/// <param name="trackChanges">if set to <see langwrod="true"/> tracks query changes.</param>
+		/// <returns>The query.</returns>
 		public static IQueryable<TEntity> TrackChanges<TEntity>(this IQueryable<TEntity> items, bool trackChanges) where TEntity : class
 			=> trackChanges ? items : items.AsNoTracking();
 	}
