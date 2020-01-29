a6ef0c2f0b6d496bb2385e00a56f227ebc31cdd9
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-01-29 14:18:18 +0000

    Updated version to 3.0.0-preview-0069

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.List``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "3.0.0-preview-0067"
-$currentBuild =  "3.0.0-preview-0068"
+$previousBuild = "3.0.0-preview-0068"
+$currentBuild =  "3.0.0-preview-0069"
 
 foreach ($file in $configFiles)
 {
