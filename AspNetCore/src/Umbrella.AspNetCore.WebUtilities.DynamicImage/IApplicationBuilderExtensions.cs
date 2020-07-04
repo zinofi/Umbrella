7882d599c7517b81d6cdaab62453f8b987a7e21e
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-07-04 13:54:47 +0100

    Added the ASP.NET Core version of the DynamicImageMiddleware.

--- AspNetCore/src/Umbrella.AspNetCore.WebUtilities.DynamicImage/IApplicationBuilderExtensions.cs
+++ AspNetCore/src/Umbrella.AspNetCore.WebUtilities.DynamicImage/IApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Umbrella.AspNetCore.WebUtilities.DynamicImage.Middleware;
+using Umbrella.DynamicImage.Abstractions;
 using Umbrella.Utilities;
 
 namespace Microsoft.AspNetCore.Builder
@@ -9,14 +10,20 @@ namespace Microsoft.AspNetCore.Builder
 	public static class IApplicationBuilderExtensions
 	{
 		/// <summary>
-		/// Adds the <see cref="Umbrella.AspNetCore.WebUtilities.DynamicImage"/> Middleware to the specified <see cref="IApplicationBuilder"/>.
+		/// Adds the <see cref="DynamicImageMiddleware"/> to the pipeline.
 		/// </summary>
-		/// <param name="builder">The application builder to which the Middleware will be added.</param>
-		/// <returns>The <see cref="IApplicationBuilder"/>.</returns>
-		/// <exception cref="ArgumentNullException">Thrown if the <paramref name="builder"/> is null.</exception>
+		/// <param name="builder">The builder.</param>
+		/// <returns>The application builder.</returns>
+		/// <remarks>
+		/// Dynamic Image URLs must take the following format: /{prefix}/{width}/{height}/{resizeMode}/{originalExtension}/{originalExtensionlessPath}.{targetExtension},
+		/// e.g. from an original URL of /images/image.jpg, the following URL will be dynamically resize the image to a .png: /dynamicimages/200/150/UniformFill/jpg/images/image.png.
+		/// <see cref="IDynamicImageUtility.GenerateVirtualPath(string, DynamicImageOptions)"/> can be used to generate application relative image URLs.
+		/// </remarks>
 		public static IApplicationBuilder UseUmbrellaDynamicImage(this IApplicationBuilder builder)
 		{
 			Guard.ArgumentNotNull(builder, nameof(builder));
+
+			builder.UseMiddleware<DynamicImageMiddleware>();
 
 			return builder;
 		}
