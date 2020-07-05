08bac4b047b326beb05b83688dd8be3c1b71a422
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-07-05 18:57:28 +0100

    Added XML code docs (WIP)

--- Core/src/Umbrella.Utilities/EmptyDisposable.cs
+++ Core/src/Umbrella.Utilities/EmptyDisposable.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Umbrella.Utilities
 {
-    public class EmptyDisposable : IDisposable
-    {
-        #region Public Static Properties
-        public static IDisposable Instance { get; } = new EmptyDisposable();
-        #endregion
+	/// <summary>
+	/// A dummy <see cref="IDisposable"/> that does nothing.
+	/// </summary>
+	/// <seealso cref="System.IDisposable" />
+	public class EmptyDisposable : IDisposable
+	{
+		#region Public Static Properties		
+		/// <summary>
+		/// Gets the instance.
+		/// </summary>
+		public static IDisposable Instance { get; } = new EmptyDisposable();
+		#endregion
 
-        #region IDisposable Members
-        public void Dispose()
-        {
-        }
-        #endregion
-    }
+		#region IDisposable Members
+		/// <inheritdoc />
+		public void Dispose()
+		{
+		}
+		#endregion
+	}
 }
--- Core/src/Umbrella.Utilities/UmbrellaStatics.cs
+++ Core/src/Umbrella.Utilities/UmbrellaStatics.cs
@@ -1,41 +1,91 @@
 ﻿using System;
 using System.Text.Json;
+using Umbrella.Utilities.Exceptions;
 
 namespace Umbrella.Utilities
 {
-    public static class UmbrellaStatics
+	/// <summary>
+	/// Static methods used by the Umbrella libraries which allow the default implementations to be changed.
+	/// </summary>
+	public static class UmbrellaStatics
     {
 		private static readonly JsonSerializerOptions _camelCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 		private static readonly JsonSerializerOptions _ignoreCaseOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-        public delegate string SerializeJsonDelegate(object value, bool useCamelCasingRules);
-        public delegate object DeserializeJsonDelegate(string value, Type type);
+		/// <summary>
+		/// The delegate definition used to perform JSON serialization.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="useCamelCasingRules">if set to <see langword="true"/>, camel case will be used for property names.</param>
+		/// <returns>The JSON string.</returns>
+		public delegate string SerializeJsonDelegate(object value, bool useCamelCasingRules);
 
+		/// <summary>
+		/// The delegate definition used to perform JSON deserialization.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="type">The type.</param>
+		/// <returns>The deserialized object.</returns>
+		public delegate object DeserializeJsonDelegate(string value, Type type);
+
+		/// <summary>
+		/// Gets or sets the JSON serializer implementation. Defaults to <see cref="DefaultSerialize(object, bool)"/>.
+		/// </summary>
 		public static SerializeJsonDelegate JsonSerializerImplementation { private get; set; } = DefaultSerialize;
+
+		/// <summary>
+		/// Gets or sets the JSON deserializer implementation. Defaults to <see cref="DefaultDeserialize(string, Type)"/>.
+		/// </summary>
 		public static DeserializeJsonDelegate JsonDeserializerImplementation { private get; set; } = DefaultDeserialize;
 
-        public static string SerializeJson(object value, bool useCamelCasingRules = false)
+		/// <summary>
+		/// Serializes the specified <paramref name="value"/> to JSON.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="useCamelCasingRules">if set to <see langword="true"/>, camel case will be used for property names.</param>
+		/// <returns>The JSON string.</returns>
+		/// <exception cref="UmbrellaException">The JsonSerializer has not been assigned. This should be done on application startup.</exception>
+		public static string SerializeJson(object value, bool useCamelCasingRules = false)
         {
             Guard.ArgumentNotNull(value, nameof(value));
 
             if(JsonSerializerImplementation == null)
-                throw new Exception("The JsonSerializer has not been assigned. This should be done on application startup.");
+                throw new UmbrellaException("The JsonSerializer has not been assigned. This should be done on application startup.");
 
             return JsonSerializerImplementation(value, useCamelCasingRules);
         }
 
-        public static T DeserializeJson<T>(string value)
+		/// <summary>
+		/// Deserializes the JSON to the specified type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type to deserialize to.</typeparam>
+		/// <param name="value">The value.</param>
+		/// <returns>The deserialized object.</returns>
+		/// <exception cref="UmbrellaException">The JsonDeserializer has not been assigned. This should be done on application startup.</exception>
+		public static T DeserializeJson<T>(string value)
         {
             Guard.ArgumentNotNullOrWhiteSpace(value, nameof(value));
 
             if(JsonDeserializerImplementation == null)
-                throw new Exception("The JsonDeserializer has not been assigned. This should be done on application startup.");
+                throw new UmbrellaException("The JsonDeserializer has not been assigned. This should be done on application startup.");
 
             return (T)JsonDeserializerImplementation(value, typeof(T));
         }
 
+		/// <summary>
+		/// Serializes the specified <paramref name="value"/> to JSON.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="useCamelCasingRules">if set to <see langword="true"/>, camel case will be used for property names.</param>
+		/// <returns>The JSON string.</returns>
 		public static string DefaultSerialize(object value, bool useCamelCasingRules) => JsonSerializer.Serialize(value, value.GetType(), useCamelCasingRules ? _camelCaseOptions : null);
 
+		/// <summary>
+		/// Deserializes the JSON to the specified type <paramref name="type"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="type">The type.</param>
+		/// <returns>The deserialized object.</returns>
 		public static object DefaultDeserialize(string value, Type type) => JsonSerializer.Deserialize(value, type, _ignoreCaseOptions);
     }
 }
