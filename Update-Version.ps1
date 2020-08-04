45c5535ee4f395cd56fc096eae08b39029917788
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-08-04 16:47:55 +0100

    Updated version to 3.0.0-preview-0117

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,8 +2,8 @@
 $affectedFiles = New-Object "System.Collections.Generic.HashSet``1[string]"
 
 # Manually alter the build number before pushing to NuGet feeds
-$previousBuild = "3.0.0-preview-0115"
-$currentBuild =  "3.0.0-preview-0116"
+$previousBuild = "3.0.0-preview-0116"
+$currentBuild =  "3.0.0-preview-0117"
 
 foreach ($file in $configFiles)
 {
