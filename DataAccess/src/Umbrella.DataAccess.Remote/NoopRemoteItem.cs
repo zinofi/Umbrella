8d8ccbeb712faba9d137cdb848cfb7c0367ae320
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-08-15 04:15:30 +0100

    Added a NoopRemoteItem type to help with cases where certain remote HTTP repository types do not exist.

---
+++ DataAccess/src/Umbrella.DataAccess.Remote/NoopRemoteItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Umbrella.DataAccess.Remote.Abstractions;
+
+namespace Umbrella.DataAccess.Remote
+{
+	/// <summary>
+	/// A <see cref="IRemoteItem{TIdentifier}"/> implementation that doesn't do anything. The <see cref="Id"/>
+	/// property will throw a <see cref="NotImplementedException"/> when accessed.
+	/// </summary>
+	/// <typeparam name="TIdentifier">The type of the identifier.</typeparam>
+	/// <seealso cref="Umbrella.DataAccess.Remote.Abstractions.IRemoteItem{TIdentifier}" />
+	public class NoopRemoteItem<TIdentifier> : IRemoteItem<TIdentifier>
+		where TIdentifier : IEquatable<TIdentifier>
+	{
+		/// <inheritdoc />
+		public TIdentifier Id => throw new NotImplementedException();
+	}
+}
