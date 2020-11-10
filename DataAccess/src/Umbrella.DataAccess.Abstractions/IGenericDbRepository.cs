8132c914127ca69c83f5ae841015952a6adb993d
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-11-10 05:49:37 +0000

    Added XML code documentation.

--- DataAccess/src/Umbrella.DataAccess.Abstractions/IGenericDbRepository.cs
+++ DataAccess/src/Umbrella.DataAccess.Abstractions/IGenericDbRepository.cs
@@ -48,17 +48,52 @@ namespace Umbrella.DataAccess.Abstractions
 		/// <param name="childOptions">The child options.</param>
 		/// <returns>An awaitable <see cref="Task"/> that completes when the operation has completed.</returns>
 		Task RemoveEmptyEntitiesAsync(ICollection<TEntity> entities, CancellationToken cancellationToken, TRepoOptions repoOptions = null, IEnumerable<RepoOptions> childOptions = null);
-		
-		
+
+		/// <summary>
+		/// Saves the entity.
+		/// </summary>
+		/// <param name="entity">The entity.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <param name="pushChangesToDb">if set to <c>true</c> pushes the changes to the database.</param>
+		/// <param name="addToContext">if set to <c>true</c> adds the entity to the database context.</param>
+		/// <param name="repoOptions">The repo options.</param>
+		/// <param name="childOptions">The child options.</param>
+		/// <param name="forceAdd">if set to <c>true</c> forces the entity to be added to the database. This is useful when the repository cannot determine if the item is new or not.</param>
+		/// <returns>The result of the save operation.</returns>
 		Task<SaveResult<TEntity>> SaveAsync(TEntity entity, CancellationToken cancellationToken = default, bool pushChangesToDb = true, bool addToContext = true, TRepoOptions repoOptions = null, IEnumerable<RepoOptions> childOptions = null, bool forceAdd = false);
-		
-		
+
+		/// <summary>
+		/// Saves all entities in the specified collection.
+		/// </summary>
+		/// <param name="entities">The entities.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <param name="pushChangesToDb">if set to <c>true</c> pushes the changes to the database.</param>
+		/// <param name="bypassSaveLogic">if set to <c>true</c> bypasses the default save logic.</param>
+		/// <param name="repoOptions">The repo options.</param>
+		/// <param name="childOptions">The child options.</param>
+		/// <returns>The result of the save operation.</returns>
 		Task<IReadOnlyCollection<SaveResult<TEntity>>> SaveAllAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool pushChangesToDb = true, bool bypassSaveLogic = false, TRepoOptions repoOptions = null, IEnumerable<RepoOptions> childOptions = null);
-		
-		
+
+		/// <summary>
+		/// Deletes the entity.
+		/// </summary>
+		/// <param name="entity">The entity.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <param name="pushChangesToDb">if set to <c>true</c> pushes the changes to the database.</param>
+		/// <param name="repoOptions">The repo options.</param>
+		/// <param name="childOptions">The child options.</param>
+		/// <returns>An awaitable <see cref="Task"/> that completes when the operation has completed.</returns>
 		Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default, bool pushChangesToDb = true, TRepoOptions repoOptions = null, IEnumerable<RepoOptions> childOptions = null);
-		
-		
+
+		/// <summary>
+		/// Deletes all of the entities in the specified collection.
+		/// </summary>
+		/// <param name="entities">The entities.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <param name="pushChangesToDb">if set to <c>true</c> pushes the changes to the database.</param>
+		/// <param name="repoOptions">The repo options.</param>
+		/// <param name="childOptions">The child options.</param>
+		/// <returns>An awaitable <see cref="Task"/> that completes when the operation has completed.</returns>
 		Task DeleteAllAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool pushChangesToDb = true, TRepoOptions repoOptions = null, IEnumerable<RepoOptions> childOptions = null);
 	}
 }
