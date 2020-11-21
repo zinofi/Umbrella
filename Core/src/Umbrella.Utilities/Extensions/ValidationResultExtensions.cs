7eca9f51d4feea8e574dc667f3c362035717b231
Richard Edwards <richard.edwards@zinofi.co.uk>
2020-11-21 00:22:05 +0000

    XML code docs.

--- Core/src/Umbrella.Utilities/Extensions/ValidationResultExtensions.cs
+++ Core/src/Umbrella.Utilities/Extensions/ValidationResultExtensions.cs
@@ -4,11 +4,22 @@ using System.Text;
 
 namespace Umbrella.Utilities.Extensions
 {
+	/// <summary>
+	/// Extension methods for use with the <see cref="ValidationResult"/> and collections if it.
+	/// </summary>
 	public static class ValidationResultExtensions
 	{
-		public static string ToValidationSummaryMessage(this IEnumerable<ValidationResult> results, string message)
+		/// <summary>
+		/// Converts the specified <paramref name="results"/> and <paramref name="introMessage"/> to a single string.
+		/// The <paramref name="introMessage"/> is displayed, followed by an empty line, followed by each message
+		/// in the <paramref name="results"/>, one per line.
+		/// </summary>
+		/// <param name="results">The results.</param>
+		/// <param name="introMessage">The introduction message displayed at the top of the validation results message.</param>
+		/// <returns>The validation message.</returns>
+		public static string ToValidationSummaryMessage(this IEnumerable<ValidationResult> results, string introMessage)
 		{
-			var messageBuilder = new StringBuilder(message).AppendLine().AppendLine();
+			var messageBuilder = new StringBuilder(introMessage).AppendLine().AppendLine();
 			results.ForEach(x => messageBuilder.AppendLine(x.ErrorMessage));
 
 			return messageBuilder.ToString();
