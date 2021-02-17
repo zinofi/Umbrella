90f5f8481730b9f5c98ea30caf07d0058fc0ec8d
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-02-17 14:39:13 +0000

    Fixes

--- Xamarin/src/Umbrella.Xamarin/Extensions/ShellExtensions.cs
+++ Xamarin/src/Umbrella.Xamarin/Extensions/ShellExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -10,7 +7,7 @@ namespace Umbrella.Xamarin.Extensions
 	/// <summary>
 	/// Extension methods for use with the <see cref="Shell"/> type.
 	/// </summary>
-    public static class ShellExtensions
+	public static class ShellExtensions
     {
 		/// <summary>
 		/// Navigates to the specified path if the page isn't already on either the
@@ -18,36 +15,37 @@ namespace Umbrella.Xamarin.Extensions
 		/// Additionally, all other pages currently on either stack are removed to ensure the page at the specified <paramref name="path"/> is always shown.
 		/// </summary>
 		/// <typeparam name="TPage">The type of the page.</typeparam>
+		/// <param name="shell">The shell instance.</param>
 		/// <param name="path">The path.</param>
 		/// <returns>An awaitable <see cref="ValueTask"/> that completes when the operation has completed.</returns>
-        public static async ValueTask GoToRootAsync<TPage>(string path)
+		public static async ValueTask GoToRootAsync<TPage>(this Shell shell, string path)
 			where TPage : Page
 		{
-			if (!Shell.Current.Navigation.ModalStack.Any(x => x is TPage) && !Shell.Current.Navigation.NavigationStack.Any(x => x is TPage))
+			if (!shell.Navigation.ModalStack.Any(x => x is TPage) && !shell.Navigation.NavigationStack.Any(x => x is TPage))
 			{
 				await Device.InvokeOnMainThreadAsync(async () =>
 				{
-					await Shell.Current.GoToAsync(path);
+					await shell.GoToAsync(path);
 
-					for (int i = 0; i < Shell.Current.Navigation.NavigationStack.Count; i++)
+					for (int i = 0; i < shell.Navigation.NavigationStack.Count; i++)
 					{
-						var page = Shell.Current.Navigation.NavigationStack[i];
+						var page = shell.Navigation.NavigationStack[i];
 
 						if (page is null || page is TPage)
 							continue;
 
-						Shell.Current.Navigation.RemovePage(page);
+						shell.Navigation.RemovePage(page);
 						i--;
 					}
 
-					for (int i = 0; i < Shell.Current.Navigation.ModalStack.Count; i++)
+					for (int i = 0; i < shell.Navigation.ModalStack.Count; i++)
 					{
-						var page = Shell.Current.Navigation.ModalStack[i];
+						var page = shell.Navigation.ModalStack[i];
 
 						if (page is null || page is TPage)
 							continue;
 
-						Shell.Current.Navigation.RemovePage(page);
+						shell.Navigation.RemovePage(page);
 						i--;
 					}
 				});
