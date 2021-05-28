a0f71e7754fb40dbcc9661cfb1cf6b1076f25a58
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-05-28 16:05:54 +0100

    Updated NuGet packages. Marked the EnumToggledConverter as obsolete.

--- Xamarin/src/Umbrella.Xamarin/Converters/EnumToggledConverter.cs
+++ Xamarin/src/Umbrella.Xamarin/Converters/EnumToggledConverter.cs
@@ -4,11 +4,14 @@ using Xamarin.Forms;
 
 namespace Umbrella.Xamarin.Converters
 {
+	// TODO: Remove this. There is a EnumToBoolConverter inside the Xamarin Community Toolkit that does the same job.
+
 	/// <summary>
 	/// A converter that converts to / from a boolean if the binding value is an enum and if so, if it matches the enum value provided as the converter parameter.
 	/// </summary>
 	/// <typeparam name="T">The enum type.</typeparam>
 	/// <seealso cref="IValueConverter" />
+	[Obsolete("There is a EnumToBoolConverter inside the Xamarin Community Toolkit that does the same job.")]
 	public class EnumToggledConverter<T> : IValueConverter
 		where T : struct, Enum
 	{
