5ab780dc05a7fcbe306d4e99b949f2254fcbd2b8
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-05-29 19:05:06 +0100

    Added the NoopPaginatedResultModel class as a convenience type.

---
+++ DataAccess/src/Umbrella.DataAccess.Remote/NoopPaginatedResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Umbrella.Utilities.Data.Pagination;
+
+namespace Umbrella.DataAccess.Remote
+{
+	/// <summary>
+	/// A <see cref="PaginatedResultModel{TItem}"/> implementation that doesn't do anything and whose items are of
+	/// type <see cref="NoopRemoteItem{TIdentifier}" />.
+	/// </summary>
+	/// <typeparam name="TIdentifier">The type of the item identifier.</typeparam>
+	/// <seealso cref="PaginatedResultModel{TItem}" />
+	public class NoopPaginatedResultModel<TIdentifier> : PaginatedResultModel<NoopRemoteItem<TIdentifier>>
+		where TIdentifier : IEquatable<TIdentifier>
+	{
+    }
+}
