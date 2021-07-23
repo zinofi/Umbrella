8491b4aae7fe3af6812a4ceae8c7f7a3d078df79
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-07-23 22:26:02 +0100

    Updated Xamarin packages. Tweak to the MaxCharSize in StackAllocConstants.

--- Core/src/Umbrella.Utilities/Constants/StackAllocConstants.cs
+++ Core/src/Umbrella.Utilities/Constants/StackAllocConstants.cs
@@ -7,8 +7,13 @@
 	public static class StackAllocConstants
 	{
 		/// <summary>
-		/// The maximum permitted size of a stack allocated char array.
+		/// The maximum permitted size of a stack allocated char array. As a char has a size of 2 bytes, this equates
+		/// to a limit of 512 bytes.
 		/// </summary>
-		public const int MaxCharSize = 128;
+		/// <remarks>
+		/// See https://vcsjones.dev/stackalloc/
+		/// and https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/stackalloc
+		/// </remarks>
+		public const int MaxCharSize = 256;
 	}
 }
