863c9b7155ec2f11deef178dabf8140393160d38
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-10-18 18:39:17 +0100

    Reverted changes to WeakEventSubscription.

--- Core/src/Umbrella.Utilities/WeakEventManager/WeakEventSubscription.cs
+++ Core/src/Umbrella.Utilities/WeakEventManager/WeakEventSubscription.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Umbrella.Utilities.WeakEventManager
@@ -15,17 +16,43 @@ namespace Umbrella.Utilities.WeakEventManager
 		public MethodInfo Handler { get; }
 
 		public override bool Equals(object? obj) => obj is WeakEventSubscription subscription && Equals(subscription);
-		public bool Equals(WeakEventSubscription other) => Subscriber.Target.Equals(other.Subscriber.Target) && Handler.Equals(other.Handler);
+		public bool Equals(WeakEventSubscription other) => EqualityComparer<WeakReference>.Default.Equals(Subscriber, other.Subscriber) && EqualityComparer<MethodInfo>.Default.Equals(Handler, other.Handler);
 
 		public override int GetHashCode()
 		{
 			int hashCode = 42314382;
-			hashCode = hashCode * -1521134295 + Subscriber.Target.GetHashCode();
-			hashCode = hashCode * -1521134295 + Handler.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<WeakReference>.Default.GetHashCode(Subscriber);
+			hashCode = hashCode * -1521134295 + EqualityComparer<MethodInfo>.Default.GetHashCode(Handler);
 			return hashCode;
 		}
 
 		public static bool operator ==(WeakEventSubscription left, WeakEventSubscription right) => left.Equals(right);
 		public static bool operator !=(WeakEventSubscription left, WeakEventSubscription right) => !(left == right);
 	}
+
+	//internal readonly struct WeakEventSubscription : IEquatable<WeakEventSubscription>
+	//{
+	//	public WeakEventSubscription(WeakReference subscriber, MethodInfo handler)
+	//	{
+	//		Subscriber = subscriber;
+	//		Handler = handler;
+	//	}
+
+	//	public WeakReference Subscriber { get; }
+	//	public MethodInfo Handler { get; }
+
+	//	public override bool Equals(object? obj) => obj is WeakEventSubscription subscription && Equals(subscription);
+	//	public bool Equals(WeakEventSubscription other) => Subscriber.Target.Equals(other.Subscriber.Target) && Handler.Equals(other.Handler);
+
+	//	public override int GetHashCode()
+	//	{
+	//		int hashCode = 42314382;
+	//		hashCode = hashCode * -1521134295 + Subscriber.Target.GetHashCode();
+	//		hashCode = hashCode * -1521134295 + Handler.GetHashCode();
+	//		return hashCode;
+	//	}
+
+	//	public static bool operator ==(WeakEventSubscription left, WeakEventSubscription right) => left.Equals(right);
+	//	public static bool operator !=(WeakEventSubscription left, WeakEventSubscription right) => !(left == right);
+	//}
 }
