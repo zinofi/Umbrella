c23bca19f76d58132615167d8a20dcb138d7fed6
Richard Edwards <richard.edwards@zinofi.co.uk>
2021-11-08 18:36:05 +0000

    Updated NuGet packages. Added new types.

---
+++ AspNetCore/src/Umbrella.AspNetCore.WebUtilities/Emails/UmbrellaRazorEmailSender.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Zinofi Digital Ltd. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbrella.AspNetCore.WebUtilities.Razor.Abstractions;
+using Umbrella.Utilities.Email.Abstractions;
+using Umbrella.WebUtilities.Exceptions;
+
+namespace Umbrella.AspNetCore.WebUtilities.Emails
+{
+	public abstract class UmbrellaRazorEmailSender
+	{
+		protected ILogger Logger { get; }
+		protected IEmailSender EmailSender { get; }
+		protected IRazorViewToStringRenderer ViewToStringRenderer { get; }
+
+		public UmbrellaRazorEmailSender(
+			ILogger logger,
+			IEmailSender emailSender,
+			IRazorViewToStringRenderer viewToStringRenderer)
+		{
+			Logger = logger;
+			EmailSender = emailSender;
+			ViewToStringRenderer = viewToStringRenderer;
+		}
+
+		protected abstract string GetFullViewPath(string viewName);
+
+		protected async Task SendEmailAsync<T>(T model, string email, string subject, string viewNameOrPath, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+
+			try
+			{
+				string viewPath = viewNameOrPath.StartsWith('~') ? viewNameOrPath : GetFullViewPath(viewNameOrPath);
+
+				string content = await ViewToStringRenderer.RenderViewToStringAsync(viewPath, model);
+
+				await EmailSender.SendEmailAsync(email, subject, content, cancellationToken);
+			}
+			catch (Exception exc) when (Logger.WriteError(exc, new { model, email, subject, viewNameOrPath }, returnValue: true))
+			{
+				throw new UmbrellaWebException($"There has been an error sending the '{subject}' email.", exc);
+			}
+		}
+	}
+}
---
+++ DataAccess/src/Umbrella.DataAccess.EntityFrameworkCore/Extensions/PropertyEntryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Umbrella.DataAccess.EntityFrameworkCore.Extensions
+{
+	/// <summary>
+	/// Extension methods for the <see cref="PropertyEntry{TEntity, TProperty}" /> class.
+	/// </summary>
+	public static class PropertyEntryExtensions
+	{
+		/// <summary>
+		/// Checks for changes to the value of a property that is being tracked by a <see cref="DbContext"/>.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <typeparam name="TProperty">The type of the property.</typeparam>
+		/// <param name="property">The property to check for changes.</param>
+		/// <returns><see langword="true"/> if the value of the <paramref name="property"/> has changed; otherwise <see langword="false"/></returns>
+		public static bool HasChanged<TEntity, TProperty>(this PropertyEntry<TEntity, TProperty> property)
+			where TEntity : class
+		{
+			TProperty currentValue = property.CurrentValue;
+			TProperty originalValue = property.OriginalValue;
+
+			if (currentValue is null && originalValue is null)
+				return false;
+
+			if (currentValue is null && originalValue != null)
+				return true;
+
+			if (currentValue != null && originalValue is null)
+				return true;
+
+			return currentValue!.Equals(originalValue) is false;
+		}
+	}
+}
