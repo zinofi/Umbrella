6b222d8095c4561ad819fe5ec76cf85350264223
Richard Edwards <richard.edwards@zinofi.co.uk>
2022-01-06 02:37:29 +0000

    Updated XML code docs (WIP)

--- Core/src/Umbrella.Utilities/WeakEventManager/Abstractions/IWeakEventManager.cs
+++ Core/src/Umbrella.Utilities/WeakEventManager/Abstractions/IWeakEventManager.cs
@@ -16,7 +16,11 @@ namespace Umbrella.Utilities.WeakEventManager.Abstractions
 		/// <param name="handler">The handler.</param>
 		/// <param name="eventName">Name of the event.</param>
 		/// <param name="state">Optional state that will be passed in as the first parameter when the <paramref name="handler"/> is invoked if the <paramref name="state"/> parameter is not null; otherwise it will not be passed in.</param>
-		/// <remarks>Because this uses <see cref="WeakReference"/> internally, be careful when using lambda expressions for the handler because they can be subject to GC if they produce closures.</remarks>
+		/// <remarks>
+		/// Because this uses <see cref="WeakReference"/> internally, be careful when using lambda expressions for the handler because they can be subject to GC if they
+		/// produce closures, i.e. Display Classes. This can be avoided by ensuring the variables captured by any lambda expressions, including nested ones, only ever
+		/// capture 'this'.
+		/// </remarks>
 		void AddEventHandler<TEventHandler>(TEventHandler handler, [CallerMemberName] string eventName = "", Dictionary<string, object>? state = null) where TEventHandler : Delegate;
 
 		/// <summary>
