363c7ffb3e40cb041d9a066e0020ccb9e5606c0f
Richard Edwards <richard.edwards@zinofi.co.uk>
2022-02-20 10:36:47 +0000

    Fixes

--- AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Grid/UmbrellaColumnFilterType.cs
+++ AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Grid/UmbrellaColumnFilterType.cs
@@ -1,4 +1,6 @@
-﻿namespace Umbrella.AspNetCore.Blazor.Components.Grid
+﻿using System.Runtime.CompilerServices;
+
+namespace Umbrella.AspNetCore.Blazor.Components.Grid
 {
 	/// <summary>
 	/// The type of the filter applied to a column.
@@ -29,5 +31,18 @@
 		/// Free text. Renders a text input of type "search".
 		/// </summary>
 		Search
+	}
+
+	internal static class UmbrellaColumnFilterTypeExtensions
+	{
+		public static string ToControlTypeString(this UmbrellaColumnFilterType value) => value switch
+		{
+			UmbrellaColumnFilterType.Text => "text",
+			UmbrellaColumnFilterType.Email => "email",
+			UmbrellaColumnFilterType.Number => "number",
+			UmbrellaColumnFilterType.Options => "text", // This will never be used but here as a default.
+			UmbrellaColumnFilterType.Search => "search",
+			_ => throw new SwitchExpressionException(value),
+		};
 	}
 }
