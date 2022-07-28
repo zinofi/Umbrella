75b6231081635cc5392b46f1fcb4abea6544dc63
Richard Edwards <richard.edwards@ultimedia.co.uk>
2022-07-28 12:19:15 +0100

    Added new AspNetCore extension methods.

---
+++ AspNetCore/src/Umbrella.AspNetCore.WebUtilities/Extensions/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Zinofi Digital Ltd. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Umbrella.AspNetCore.WebUtilities.Extensions;
+
+/// <summary>
+/// Extension methods that operate on <see cref="ModelStateDictionary" /> instances.
+/// </summary>
+public static class ModelStateDictionaryExtensions
+{
+	/// <summary>
+	/// Adds the validation results to the model state dictionary.
+	/// </summary>
+	/// <param name="modelState">The model state dictionary.</param>
+	/// <param name="validationResults">The validation results.</param>
+	public static void AddValidationResults(this ModelStateDictionary modelState, IEnumerable<ValidationResult> validationResults)
+	{
+		foreach (var item in validationResults)
+		{
+			var memberNames = item.MemberNames?.ToList();
+
+			if (memberNames?.Count > 0)
+			{
+				foreach (string key in memberNames)
+				{
+					modelState.AddModelError(key, item.ErrorMessage ?? string.Empty);
+				}
+			}
+			else
+			{
+				if (!string.IsNullOrEmpty(item.ErrorMessage))
+					modelState.AddModelError(string.Empty, item.ErrorMessage);
+			}
+		}
+	}
+}
