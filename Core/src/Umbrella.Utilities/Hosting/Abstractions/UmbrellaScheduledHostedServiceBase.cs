33d18d4ede2b4887528cab8c61552888d91e5770
Richard Edwards <richard.edwards@ultimedia.co.uk>
2022-07-28 14:00:09 +0100

    Changes to the UmbrellaScheduledHostedServiceBase to allow the schedule string to be loaded asynchronously.

--- Core/src/Umbrella.Utilities/Hosting/Abstractions/UmbrellaScheduledHostedServiceBase.cs
+++ Core/src/Umbrella.Utilities/Hosting/Abstractions/UmbrellaScheduledHostedServiceBase.cs
@@ -5,9 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NCrontab;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Umbrella.Utilities.Exceptions;
 using Umbrella.Utilities.Threading.Abstractions;
 
@@ -21,8 +18,6 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 	public abstract class UmbrellaScheduledHostedServiceBase : IHostedService, IDisposable
 	{
 		private CancellationTokenSource? _cancellationTokenSource;
-		private readonly CrontabSchedule _crontabSchedule;
-		private DateTime _nextRun;
 
 		/// <summary>
 		/// Gets the logger.
@@ -40,14 +35,6 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 		protected ISynchronizationManager SynchronizationManager { get; }
 
 		/// <summary>
-		/// Gets the crontab schedule in UTC format which determines when this services will be executed.
-		/// </summary>
-		/// <remarks>
-		/// See https://ncrontab.swimburger.net/ to test compatible expressions.
-		/// </remarks>
-		protected abstract string CrontabScheduleUtcString { get; }
-
-		/// <summary>
 		/// Initializes a new instance of the <see cref="UmbrellaScheduledHostedServiceBase"/> class.
 		/// </summary>
 		/// <param name="logger">The logger.</param>
@@ -61,15 +48,10 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 			Logger = logger;
 			ServiceScopeFactory = serviceScopeFactory;
 			SynchronizationManager = synchronizationManager;
-
-			_crontabSchedule = CrontabSchedule.Parse(CrontabScheduleUtcString, new CrontabSchedule.ParseOptions { IncludingSeconds = true });
-			_nextRun = _crontabSchedule.GetNextOccurrence(DateTime.UtcNow);
-
-			Logger.WriteInformation(new { _nextRun }, "First run is scheduled.");
 		}
 
 		/// <inheritdoc />
-		public Task StartAsync(CancellationToken cancellationToken)
+		public async Task StartAsync(CancellationToken cancellationToken)
 		{
 			cancellationToken.ThrowIfCancellationRequested();
 
@@ -77,11 +59,17 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 			{
 				_cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
-				Task.Run(async () =>
+				string scheduleString = await GetCrontabScheduleUtcStringAsync(cancellationToken);
+				var crontabSchedule = CrontabSchedule.Parse(scheduleString, new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+				DateTime nextRun = crontabSchedule.GetNextOccurrence(DateTime.UtcNow);
+
+				Logger.WriteInformation(new { nextRun }, "First run is scheduled.");
+
+				_ = Task.Run(async () =>
 				{
 					while (!cancellationToken.IsCancellationRequested)
 					{
-						await Task.Delay(Math.Max(0, (int)_nextRun.Subtract(DateTime.UtcNow).TotalMilliseconds));
+						await Task.Delay(Math.Max(0, (int)nextRun.Subtract(DateTime.UtcNow).TotalMilliseconds));
 
 						Logger.WriteInformation(new { Date = DateTime.UtcNow }, "Running.");
 
@@ -91,21 +79,19 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 
 							await ExecuteAsync(serviceScope, cancellationToken);
 						}
-						catch (Exception exc) when (!(exc is OperationCanceledException) && Logger.WriteError(exc, returnValue: true))
+						catch (Exception exc) when (exc is not OperationCanceledException && Logger.WriteError(exc))
 						{
 							// Mask the exception to allow the job to retry on the next run
 						}
 
-						_nextRun = _crontabSchedule.GetNextOccurrence(DateTime.UtcNow);
+						nextRun = crontabSchedule.GetNextOccurrence(DateTime.UtcNow);
 
-						Logger.WriteInformation(new { _nextRun }, "Next run is scheduled.");
+						Logger.WriteInformation(new { nextRun }, "Next run is scheduled.");
 					}
 				},
 				cancellationToken);
-
-				return Task.CompletedTask;
 			}
-			catch (Exception exc) when (!(exc is OperationCanceledException) && Logger.WriteError(exc, returnValue: true))
+			catch (Exception exc) when (exc is not OperationCanceledException && Logger.WriteError(exc))
 			{
 				throw new UmbrellaException("There has been a problem with this service during startup.", exc);
 			}
@@ -131,5 +117,12 @@ namespace Umbrella.Utilities.Hosting.Abstractions
 		/// <param name="cancellationToken">The cancellation token.</param>
 		/// <returns>An awaitable <see cref="Task"/> used to await execution.</returns>
 		protected abstract Task ExecuteAsync(IServiceScope serviceScope, CancellationToken cancellationToken);
+
+		/// <summary>
+		/// Gets the crontab schedule string.
+		/// </summary>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>The schedule string.</returns>
+		protected abstract Task<string> GetCrontabScheduleUtcStringAsync(CancellationToken cancellationToken);
 	}
 }
