f2b36f3e0d73242de06ea25cb30468a2a677e49e
Richard Edwards <richard.edwards@zinofi.co.uk>
2022-08-16 18:12:01 +0100

    Fixes Updated version to 6.0.0-preview-0009

--- FileSystem/src/Umbrella.FileSystem.Abstractions/UmbrellaFileProviderMapping.cs
+++ FileSystem/src/Umbrella.FileSystem.Abstractions/UmbrellaFileProviderMapping.cs
@@ -1,65 +1,65 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿// Copyright (c) Zinofi Digital Ltd. All Rights Reserved.
+// Licensed under the MIT License.
+
 using Umbrella.Utilities;
 using Umbrella.Utilities.Extensions;
 using Umbrella.Utilities.Options.Abstractions;
 
-namespace Umbrella.FileSystem.Abstractions
+namespace Umbrella.FileSystem.Abstractions;
+
+/// <summary>
+/// Specifies a mapping between a collection of application relative folder paths and a <see cref="IUmbrellaFileProvider"/> instance.
+/// </summary>
+/// <seealso cref="ISanitizableUmbrellaOptions" />
+/// <seealso cref="IValidatableUmbrellaOptions" />
+public class UmbrellaFileProviderMapping : ISanitizableUmbrellaOptions, IValidatableUmbrellaOptions
 {
 	/// <summary>
-	/// Specifies a mapping between a collection of application relative folder paths and a <see cref="IUmbrellaFileProvider"/> instance.
+	/// Initializes a new instance of the <see cref="UmbrellaFileProviderMapping"/> class.
 	/// </summary>
-	/// <seealso cref="ISanitizableUmbrellaOptions" />
-	/// <seealso cref="IValidatableUmbrellaOptions" />
-	public class UmbrellaFileProviderMapping : ISanitizableUmbrellaOptions, IValidatableUmbrellaOptions
+	/// <param name="fileProvider">The file provider.</param>
+	/// <param name="appRelativeFolderPaths">The application relative folder paths.</param>
+	public UmbrellaFileProviderMapping(IUmbrellaFileProvider fileProvider, params string[] appRelativeFolderPaths)
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="UmbrellaFileProviderMapping"/> class.
-		/// </summary>
-		/// <param name="fileProvider">The file provider.</param>
-		/// <param name="appRelativeFolderPaths">The application relative folder paths.</param>
-		public UmbrellaFileProviderMapping(IUmbrellaFileProvider fileProvider, params string[] appRelativeFolderPaths)
+		FileProvider = fileProvider;
+		AppRelativeFolderPaths = appRelativeFolderPaths;
+	}
+
+	/// <summary>
+	/// Gets the file provider.
+	/// </summary>
+	public IUmbrellaFileProvider FileProvider { get; }
+
+	/// <summary>
+	/// Gets the application relative folder paths.
+	/// </summary>
+	public IReadOnlyCollection<string> AppRelativeFolderPaths { get; private set; }
+
+	/// <summary>
+	/// Sanitizes this instance.
+	/// </summary>
+	public void Sanitize()
+	{
+		if (AppRelativeFolderPaths != null)
 		{
-			FileProvider = fileProvider;
-			AppRelativeFolderPaths = appRelativeFolderPaths;
+			// When sanitizing the paths, ensure they all have a trailing '/'.
+			// This is to avoid an issue encountered when matching paths that start with the same name,
+			// e.g. /folder-cool and /folder-coolio would both be matched. Adding a trailing '/' resolves this.
+			AppRelativeFolderPaths = AppRelativeFolderPaths
+				.Where(x => !string.IsNullOrWhiteSpace(x))
+				.Select(x => $"/{x.TrimToLowerInvariant().Trim().Trim('/').TrimEnd('/')}/")
+				.Distinct()
+				.ToList()
+				.AsReadOnly();
 		}
+	}
 
-		/// <summary>
-		/// Gets the file provider.
-		/// </summary>
-		public IUmbrellaFileProvider FileProvider { get; }
-
-		/// <summary>
-		/// Gets the application relative folder paths.
-		/// </summary>
-		public IReadOnlyCollection<string> AppRelativeFolderPaths { get; private set; }
-
-		/// <summary>
-		/// Sanitizes this instance.
-		/// </summary>
-		public void Sanitize()
-		{
-			if (AppRelativeFolderPaths != null)
-			{
-				// When sanitizing the paths, ensure they all have a trailing '/'.
-				// This is to avoid an issue encountered when matching paths that start with the same name,
-				// e.g. /folder-cool and /folder-coolio would both be matched. Adding a trailing '/' resolves this.
-				AppRelativeFolderPaths = AppRelativeFolderPaths
-					.Where(x => !string.IsNullOrWhiteSpace(x) && x[0] == '/')
-					.Select(x => x.TrimToLowerInvariant().TrimEnd('/') + "/")
-					.Distinct()
-					.ToList()
-					.AsReadOnly();
-			}
-		}
-
-		/// <summary>
-		/// Validates this instance.
-		/// </summary>
-		public void Validate()
-		{
-			Guard.ArgumentNotNull(FileProvider, nameof(FileProvider));
-			Guard.ArgumentNotNullOrEmpty(AppRelativeFolderPaths, nameof(AppRelativeFolderPaths));
-		}
+	/// <summary>
+	/// Validates this instance.
+	/// </summary>
+	public void Validate()
+	{
+		Guard.ArgumentNotNull(FileProvider, nameof(FileProvider));
+		Guard.ArgumentNotNullOrEmpty(AppRelativeFolderPaths, nameof(AppRelativeFolderPaths));
 	}
 }
