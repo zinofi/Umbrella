9b186f779e38f49830af352dc90f49728618b966
Richard Edwards <richard.edwards@zinofi.co.uk>
2022-08-21 15:45:44 +0100

    Added the capability to add a heading to the UmbrellaValidationSummary. Updated version to 3.0.0-preview-0301

--- AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Validation/UmbrellaValidationSummary.razor.cs
+++ AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Validation/UmbrellaValidationSummary.razor.cs
@@ -7,9 +7,15 @@ namespace Umbrella.AspNetCore.Blazor.Components.Validation
 	/// <summary>
 	/// A component used to display a collection of <see cref="ValidationResult"/> instances.
 	/// </summary>
-	/// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+	/// <seealso cref="ComponentBase" />
 	public partial class UmbrellaValidationSummary
 	{
+		/// <summary>
+		/// Gets or sets the heading.
+		/// </summary>
+		[Parameter]
+		public string? Heading { get; set; }
+
 		/// <summary>
 		/// Gets or sets the validation results.
 		/// </summary>
