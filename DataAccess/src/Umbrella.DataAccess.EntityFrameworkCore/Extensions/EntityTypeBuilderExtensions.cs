da4e14a0115301e73d3bba95a05405111815b711
Richard Edwards <richard.edwards@ultimedia.co.uk>
2022-09-14 11:49:33 +0100

    Added new EntityTypeBuilder extensions to make setting up audit properties easier.

--- DataAccess/src/Umbrella.DataAccess.EntityFrameworkCore/Extensions/EntityTypeBuilderExtensions.cs
+++ DataAccess/src/Umbrella.DataAccess.EntityFrameworkCore/Extensions/EntityTypeBuilderExtensions.cs
@@ -1,21 +1,55 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿// Copyright (c) Zinofi Digital Ltd. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Umbrella.DataAccess.Abstractions;
 using Umbrella.Utilities.Data.Concurrency;
 
-namespace Umbrella.DataAccess.EntityFrameworkCore.Extensions
+namespace Umbrella.DataAccess.EntityFrameworkCore.Extensions;
+
+/// <summary>
+/// Extension methods for the <see cref="EntityTypeBuilder{T}" /> class.
+/// </summary>
+public static class EntityTypeBuilderExtensions
 {
 	/// <summary>
-	/// Extension methods for the <see cref="EntityTypeBuilder{T}" /> class.
+	/// Setups the concurrency token.
 	/// </summary>
-	public static class EntityTypeBuilderExtensions
+	/// <typeparam name="TEntity">The type of the entity.</typeparam>
+	/// <param name="builder">The builder.</param>
+	/// <returns>The builder.</returns>
+	public static PropertyBuilder<string> SetupConcurrencyToken<TEntity>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, IConcurrencyStamp
+		=> builder.Property(x => x.ConcurrencyStamp).HasMaxLength(36).IsRequired().IsConcurrencyToken();
+
+	public static PropertyBuilder<DateTime> SetupCreatedDateUtcAuditProperty<TEntity>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, ICreatedDateAuditEntity
+		=> builder.Property(x => x.CreatedDateUtc).EnsureUtc();
+
+	public static PropertyBuilder<DateTime> SetupUpdatedDateUtcAuditProperty<TEntity>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, IUpdatedDateAuditEntity
+		=> builder.Property(x => x.UpdatedDateUtc).EnsureUtc();
+
+	public static ReferenceCollectionBuilder<TAppUserEntity, TEntity> SetupCreatedByIdAuditProperty<TEntity, TAppUserEntity, TUserId>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, ICreatedUserAuditEntity<TUserId>
+		where TAppUserEntity : class
+		=> builder.HasOne<TAppUserEntity>().WithMany().HasForeignKey(x => x.CreatedById).OnDelete(DeleteBehavior.Restrict);
+
+	public static ReferenceCollectionBuilder<TAppUserEntity, TEntity> SetupUpdatedByIdAuditProperty<TEntity, TAppUserEntity, TUserId>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, IUpdatedUserAuditEntity<TUserId>
+		where TAppUserEntity : class
+		=> builder.HasOne<TAppUserEntity>().WithMany().HasForeignKey(x => x.UpdatedById).OnDelete(DeleteBehavior.Restrict);
+
+	public static EntityTypeBuilder<TEntity> SetupAuditProperties<TEntity, TAppUserEntity, TUserId>(this EntityTypeBuilder<TEntity> builder)
+		where TEntity : class, ICreatedDateAuditEntity, IUpdatedDateAuditEntity, ICreatedUserAuditEntity<TUserId>, IUpdatedUserAuditEntity<TUserId>
+		where TAppUserEntity : class
 	{
-		/// <summary>
-		/// Setups the concurrency token.
-		/// </summary>
-		/// <typeparam name="TEntity">The type of the entity.</typeparam>
-		/// <param name="builder">The builder.</param>
-		/// <returns>The builder.</returns>
-		public static PropertyBuilder<string> SetupConcurrencyToken<TEntity>(this EntityTypeBuilder<TEntity> builder)
-			where TEntity : class, IConcurrencyStamp
-			=> builder.Property(x => x.ConcurrencyStamp).HasMaxLength(36).IsRequired().IsConcurrencyToken();
+		_ = SetupCreatedDateUtcAuditProperty(builder);
+		_ = SetupUpdatedDateUtcAuditProperty(builder);
+		_ = SetupCreatedByIdAuditProperty<TEntity, TAppUserEntity, TUserId>(builder);
+		_ = SetupUpdatedByIdAuditProperty<TEntity, TAppUserEntity, TUserId>(builder);
+
+		return builder;
 	}
 }
