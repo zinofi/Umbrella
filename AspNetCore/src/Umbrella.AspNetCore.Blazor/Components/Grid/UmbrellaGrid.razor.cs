92291bc2f54398d56a3bfaad02030a550e732c7c
Richard Edwards <richard.edwards@zinofi.co.uk>
2022-09-25 19:31:38 +0100

    Exposed the Items property getter as protected

--- AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Grid/UmbrellaGrid.razor.cs
+++ AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/Grid/UmbrellaGrid.razor.cs
@@ -125,7 +125,7 @@ public partial class UmbrellaGrid<TItem> : IUmbrellaGrid<TItem>
 	/// <summary>
 	/// Gets or sets the items collection displayed in the grid.
 	/// </summary>
-	private IReadOnlyCollection<TItem> Items { get; set; } = Array.Empty<TItem>();
+	protected IReadOnlyCollection<TItem> Items { get; private set; } = Array.Empty<TItem>();
 
 	/// <summary>
 	/// Gets or sets the message displayed when the grid is loading.
