f804c3f44b39630fdc86e51deb93beef68f01581
Richard Edwards <richard.edwards@zinofi.co.uk>
2023-02-02 12:29:52 +0000

    Initial work on a form layout state component (WIP)

---
+++ AspNetCore/src/Umbrella.AspNetCore.Blazor/Components/FormLayoutStateView/UmbrellaFormLayoutStateView.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbrella.AspNetCore.Blazor.Enumerations;
+
+namespace Umbrella.AspNetCore.Blazor.Components.FormLayoutStateView;
+
+public partial class UmbrellaFormLayoutStateView
+{
+	[Parameter]
+	[EditorRequired]
+	public LayoutState CurrentState { get; set; } = LayoutState.Loading;
+
+	[Parameter]
+	public object? Model { get; set; }
+
+	[Parameter]
+	public EventCallback ReloadCallback { get; set; }
+
+	[Parameter]
+	[EditorRequired]
+	public RenderFragment? Success { get; set; }
+
+	[Parameter]
+	public RenderFragment? Loading { get; set; }
+
+	[Parameter]
+	public RenderFragment? Error { get; set; }
+}
