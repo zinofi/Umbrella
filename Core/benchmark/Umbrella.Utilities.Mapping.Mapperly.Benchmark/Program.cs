c3a11941612eeb58b3e9e48ec91cee6c54ec73fe
Richard Edwards <richard.edwards@ultimedia.agency>
2023-10-22 15:50:27 +0100

    Cleanup in and addition of benchmark project for Mapperly.

---
+++ Core/benchmark/Umbrella.Utilities.Mapping.Mapperly.Benchmark/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<UmbrellaMapperBenchmark>();
+
+Console.WriteLine("Press any key to exit...");
+_ = Console.Read();
---
+++ Core/benchmark/Umbrella.Utilities.Mapping.Mapperly.Benchmark/UmbrellaMapperBenchmark.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Umbrella.Utilities.Mapping.Mapperly.Benchmark;
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net461), SimpleJob(RuntimeMoniker.Net60)]
+public class UmbrellaMapperBenchmark
+{
+	
+}
