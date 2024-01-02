e4cbb6a0e47bf1c30c60bbcb71880d36d74c87e4
Richard Edwards <richard.edwards@ultimedia.agency>
2024-01-02 11:43:57 +0000

    Updated NuGet packages. Added a new Combine method to the IncludeMap class. Added unit tests.

--- DataAccess/src/Umbrella.DataAccess.Abstractions/IncludeMap.cs
+++ DataAccess/src/Umbrella.DataAccess.Abstractions/IncludeMap.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 using Umbrella.Utilities.Extensions;
 
 namespace Umbrella.DataAccess.Abstractions;
@@ -9,34 +10,86 @@ namespace Umbrella.DataAccess.Abstractions;
 /// <typeparam name="TEntity">The type of the entity.</typeparam>
 public class IncludeMap<TEntity>
 {
+	private class IncludeMapExpressionEqualityComparer : EqualityComparer<Expression<Func<TEntity, object?>>>
+	{
+		public override bool Equals(Expression<Func<TEntity, object?>>? x, Expression<Func<TEntity, object?>>? y)
+		{
+			// If both are null, or both are same instance, return true.
+			if (ReferenceEquals(x, y))
+				return true;
+
+			// If one is null, but not both, return false.
+			if (x is null || y is null)
+				return false;
+
+			// If the types are different, return false.
+			if (x.NodeType != y.NodeType || x.Type != y.Type)
+				return false;
+
+			return x.GetMemberPath() == y.GetMemberPath();
+		}
+
+#if NET6_0_OR_GREATER
+		public override int GetHashCode([DisallowNull] Expression<Func<TEntity, object?>> obj) => obj.GetMemberPath().GetHashCode(StringComparison.InvariantCulture);
+#else
+		public override int GetHashCode([DisallowNull] Expression<Func<TEntity, object?>> obj) => obj.GetMemberPath().GetHashCode();
+#endif
+	}
+
+	private readonly static IncludeMapExpressionEqualityComparer _includeMapExpressionEqualityComparer = new();
+
 	/// <summary>
 	/// Gets the includes as expressions.
 	/// </summary>
-	public HashSet<Expression<Func<TEntity, object?>>> Includes { get; } = [];
+	public IReadOnlyCollection<Expression<Func<TEntity, object?>>> Includes { get; }
 
 	/// <summary>
 	/// Gets the includes as string paths, e.g. Parent -> Child -> Name will be Parent.Child.Name.
 	/// </summary>
-	public HashSet<string> PropertyPaths { get; } = [];
-	
+	public IReadOnlyCollection<string> PropertyPaths { get; }
+
+	private IncludeMap()
+	{
+		Includes = new HashSet<Expression<Func<TEntity, object?>>>();
+		PropertyPaths = new HashSet<string>();
+	}
+
 	/// <summary>
 	/// Initializes a new instance of the <see cref="IncludeMap{TEntity}"/> class.
 	/// </summary>
 	/// <param name="paths">The property paths.</param>
 	public IncludeMap(params Expression<Func<TEntity, object?>>[] paths)
 	{
+		var includes = new HashSet<Expression<Func<TEntity, object?>>>(_includeMapExpressionEqualityComparer);
+		var propertyPaths = new HashSet<string>(StringComparer.InvariantCulture);
+
+		Includes = includes;
+		PropertyPaths = propertyPaths;
+
 		if (paths is null)
 			return;
 
 		foreach (var path in paths)
 		{
-			_ = Includes.Add(path);
+			_ = includes.Add(path);
 
 			string propertyPath = path.GetMemberPath();
 
 			if (!string.IsNullOrEmpty(propertyPath))
-				_ = PropertyPaths.Add(propertyPath);
+				_ = propertyPaths.Add(propertyPath);
 		}
+	}
+
+	/// <summary>
+	/// Combines the specified includes with the <see cref="Includes"/> property of the current instance.
+	/// </summary>
+	/// <param name="includes">The includes.</param>
+	/// <returns>A new combined <see cref="IncludeMap{TEntity}"/> instance.</returns>
+	public IncludeMap<TEntity> Combine(params IncludeMap<TEntity>[] includes)
+	{
+		var x = includes.Select(x => x.Includes).SelectMany(x => x).Concat(Includes).ToArray();
+
+		return new IncludeMap<TEntity>(x);
 	}
 
 	/// <inheritdoc />
--- DataAccess/test/Umbrella.DataAccess.Abstractions.Test/IncludeMapTest.cs
+++ DataAccess/test/Umbrella.DataAccess.Abstractions.Test/IncludeMapTest.cs
@@ -50,4 +50,48 @@ public class IncludeMapTest
 		// String
 		Assert.Equal(string.Join(", ", propertyPaths), map.ToString());
 	}
+
+	[Fact]
+	public void Duplicates_Invalid()
+	{
+		var expressions = new Expression<Func<Type, object?>>[]
+		{
+			x => x.Name,
+			x => x.Assembly,
+			x => x.Assembly.Location,
+			x => x.Assembly.ImageRuntimeVersion.Length,
+			x => x.Assembly.ExportedTypes.Select(x => x.FullName),
+			x => x.Assembly.ExportedTypes.Select(x => x.FullName!.Length),
+			x => x.GenericTypeArguments.Select(x => x.FullName),
+			x => x.GenericTypeArguments.Select(x => x.Assembly.FullName!.Length),
+			x => x.GenericTypeArguments.Select(x => x.Assembly.FullName!.Length)
+		};
+
+		var map = new IncludeMap<Type>(expressions);
+
+		var validExpressions = expressions.SkipLast(1).ToArray();
+
+		// Includes
+		Assert.Equal(validExpressions.Length, map.Includes.Count);
+		Assert.True(validExpressions.SequenceEqual(map.Includes));
+
+		// Property Paths
+		string[] propertyPaths =
+		[
+			"Name",
+			"Assembly",
+			"Assembly.Location",
+			"Assembly.ImageRuntimeVersion.Length",
+			"Assembly.ExportedTypes.FullName",
+			"Assembly.ExportedTypes.FullName.Length",
+			"GenericTypeArguments.FullName",
+			"GenericTypeArguments.Assembly.FullName.Length"
+		];
+
+		Assert.Equal(propertyPaths.Length, map.PropertyPaths.Count);
+		Assert.True(propertyPaths.SequenceEqual(map.PropertyPaths));
+
+		// String
+		Assert.Equal(string.Join(", ", propertyPaths), map.ToString());
+	}
 }
