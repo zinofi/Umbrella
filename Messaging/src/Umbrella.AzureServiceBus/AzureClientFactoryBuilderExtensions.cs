39c921249bae3ed2533dcde9f90923adba3409c4
Richard Edwards <richard.edwards@ultimedia.agency>
2024-01-25 11:26:26 +0000

    Added extension methods for setting up the Azure Service Bus client. Updated version to 6.0.0-preview-0279

---
+++ Messaging/src/Umbrella.AzureServiceBus/AzureClientFactoryBuilderExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Core.Extensions;
+using Azure.Messaging.ServiceBus;
+using CommunityToolkit.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Umbrella.AzureServiceBus;
+using Umbrella.AzureServiceBus.Abstractions;
+using Umbrella.AzureServiceBus.Plugins;
+
+#pragma warning disable IDE0130
+namespace Microsoft.Extensions.Azure;
+
+/// <summary>
+/// Extension methods for adding the Umbrella Azure Service Bus client to the <see cref="AzureClientFactoryBuilder"/> builder.
+/// </summary>
+public static class AzureClientFactoryBuilderExtensions
+{
+	/// <summary>
+	/// Adds the Umbrella Azure Service Bus client to the <see cref="AzureClientFactoryBuilder"/> builder.
+	/// </summary>
+	/// <param name="builder">The builder.</param>
+	/// <param name="loggerFactory">The logger factory.</param>
+	/// <param name="claimCheckStorageConnectionString">The claim check storage connection string.</param>
+	/// <param name="claimCheckStorageContainerName">The claim check storage container name.</param>
+	/// <param name="serviceBusConnectionString">The service bus connection string.</param>
+	/// <returns>The <see cref="IAzureClientBuilder{UmbrellaAzureServiceBusClient, ServiceBusClientOptions}"/> client builder.</returns>
+	public static IAzureClientBuilder<UmbrellaAzureServiceBusClient, ServiceBusClientOptions> AddUmbrellaAzureServiceBusClient(
+		this AzureClientFactoryBuilder builder,
+		ILoggerFactory loggerFactory,
+		string claimCheckStorageConnectionString,
+		string claimCheckStorageContainerName,
+		string serviceBusConnectionString)
+	{
+		Guard.IsNotNull(builder);
+
+		return builder.AddClient<UmbrellaAzureServiceBusClient, ServiceBusClientOptions>(options =>
+		{
+			// Define Plugins
+			IReadOnlyCollection<IUmbrellaAzureServiceBusPlugin> plugins =
+			[
+				new UmbrellaAzureServiceBusClaimCheckPlugin(
+					loggerFactory.CreateLogger<UmbrellaAzureServiceBusClaimCheckPlugin>(),
+					claimCheckStorageConnectionString,
+					claimCheckStorageContainerName)
+			];
+
+			return new UmbrellaAzureServiceBusClient(
+				loggerFactory,
+				serviceBusConnectionString,
+				options,
+				plugins: plugins);
+		})
+		.AddUmbrellaRetryOptions();
+	}
+}
---
+++ Messaging/src/Umbrella.AzureServiceBus/IAzureClientBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Azure;
+
+#pragma warning disable IDE0130
+namespace Azure.Core.Extensions;
+
+/// <summary>
+/// Extension methods used to add Umbrella retry options to the <see cref="IAzureClientBuilder{TClient,TOptions}"/> client builder.
+/// </summary>
+public static class IAzureClientBuilderExtensions
+{
+	/// <summary>
+	/// Adds the Umbrella retry options to the <see cref="IAzureClientBuilder{TClient,TOptions}"/> client builder.
+	/// </summary>
+	/// <typeparam name="TClient">The type of the client.</typeparam>
+	/// <typeparam name="TOptions">The type of the options.</typeparam>
+	/// <param name="builder">The builder.</param>
+	/// <param name="retryMode">The retry mode.</param>
+	/// <param name="maxRetries">The maximum retries.</param>
+	/// <param name="delayInMilliseconds">The delay in milliseconds.</param>
+	/// <param name="maxDelayInMilliseconds">The maximum delay in milliseconds.</param>
+	/// <param name="tryTimeoutInMilliseconds">The try timeout in milliseconds.</param>
+	/// <returns>The <see cref="IAzureClientBuilder{TClient,TOptions}"/> client builder.</returns>
+	public static IAzureClientBuilder<TClient, TOptions> AddUmbrellaRetryOptions<TClient, TOptions>(
+		this IAzureClientBuilder<TClient, TOptions> builder,
+		ServiceBusRetryMode retryMode = ServiceBusRetryMode.Exponential,
+		int maxRetries = 3,
+		int delayInMilliseconds = 5000,
+		int maxDelayInMilliseconds = 30000,
+		int tryTimeoutInMilliseconds = 30000)
+		where TClient : class
+		where TOptions : ServiceBusClientOptions
+	{
+		return builder.ConfigureOptions(x =>
+		{
+			x.RetryOptions = new ServiceBusRetryOptions
+			{
+				Mode = retryMode,
+				MaxRetries = maxRetries,
+				Delay = TimeSpan.FromMilliseconds(delayInMilliseconds),
+				MaxDelay = TimeSpan.FromMilliseconds(maxDelayInMilliseconds),
+				TryTimeout = TimeSpan.FromMilliseconds(tryTimeoutInMilliseconds)
+			};
+		});
+	}
+}
