ed898df6dfe3ca7ca7a08e9ffc35e0131f960ae8
Richard Edwards <richard.edwards@ultimedia.agency>
2024-06-03 13:14:03 +0100

    Fixes

--- AspNetCore/src/Umbrella.AspNetCore.WebUtilities.DataProtection/IDataProtectionBuilderExtensions.cs
+++ AspNetCore/src/Umbrella.AspNetCore.WebUtilities.DataProtection/IDataProtectionBuilderExtensions.cs
@@ -12,35 +12,39 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class IDataProtectionBuilderExtensions
 {
-    /// <summary>
-    /// Persists the data protection keys to Azure Blob Storage and protects them with Azure Key Vault.
-    /// </summary>
-    /// <param name="builder">The <see cref="IDataProtectionBuilder"/> instance.</param>
-    /// <param name="keyVaultName">The name of the Azure Key Vault.</param>
-    /// <param name="storageConnectionString">The connection string for the Azure Blob Storage.</param>
-    /// <param name="dpapiKeysContainerName">The name of the container in Azure Blob Storage to store the keys.</param>
-    /// <param name="keysFileName">The name of the file in the container to store the keys.</param>
-    /// <returns>The <see cref="IDataProtectionBuilder"/> instance.</returns>
-    public static async Task<IDataProtectionBuilder> PersistKeysToBlobStorageAndProtectWithKeyVaultAsync(
-        this IDataProtectionBuilder builder,
-        string? keyVaultName,
-        string? storageConnectionString,
-        string dpapiKeysContainerName = "dpapi-keys",
-        string keysFileName = "keys.xml")
-    {
-        Guard.IsNotNull(builder);
-        Guard.IsNotNullOrEmpty(keyVaultName);
-        Guard.IsNotNullOrEmpty(storageConnectionString);
+	/// <summary>
+	/// Persists the data protection keys to Azure Blob Storage and protects them with Azure Key Vault.
+	/// </summary>
+	/// <param name="builder">The <see cref="IDataProtectionBuilder"/> instance.</param>
+	/// <param name="keyVaultName">The name of the Azure Key Vault.</param>
+	/// <param name="storageConnectionString">The connection string for the Azure Blob Storage.</param>
+	/// <param name="dpapiKeysContainerName">The name of the container in Azure Blob Storage to store the keys.</param>
+	/// <param name="keysFileName">The name of the file in the container to store the keys.</param>
+	/// <returns>The <see cref="IDataProtectionBuilder"/> instance.</returns>
+	/// <remarks>
+	/// The specified Azure Blob Storage container will be created if it does not already exist.
+	/// The keys file will also be created if it does not already exist in the container with an empty repository element.
+	/// </remarks>
+	public static async Task<IDataProtectionBuilder> PersistKeysToBlobStorageAndProtectWithKeyVaultAsync(
+		this IDataProtectionBuilder builder,
+		string? keyVaultName,
+		string? storageConnectionString,
+		string dpapiKeysContainerName = "dpapi-keys",
+		string keysFileName = "keys.xml")
+	{
+		Guard.IsNotNull(builder);
+		Guard.IsNotNullOrEmpty(keyVaultName);
+		Guard.IsNotNullOrEmpty(storageConnectionString);
 
-        BlobServiceClient serviceClient = new(storageConnectionString);
-        BlobContainerClient container = serviceClient.GetBlobContainerClient(dpapiKeysContainerName);
-        BlobClient blobClient = container.GetBlobClient(keysFileName);
+		BlobServiceClient serviceClient = new(storageConnectionString);
+		BlobContainerClient container = serviceClient.GetBlobContainerClient(dpapiKeysContainerName);
+		BlobClient blobClient = container.GetBlobClient(keysFileName);
 
-		await container.CreateIfNotExistsAsync().ConfigureAwait(false);
+		_ = await container.CreateIfNotExistsAsync().ConfigureAwait(false);
 
 		if (!await blobClient.ExistsAsync().ConfigureAwait(false))
 		{
-			await blobClient.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(
+			_ = await blobClient.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(
 				"""
 				<?xml version="1.0" encoding="utf-8"?>
 				<repository>
@@ -49,7 +53,7 @@ public static class IDataProtectionBuilderExtensions
 		}
 
 		return builder
-            .PersistKeysToAzureBlobStorage(blobClient)
-            .ProtectKeysWithAzureKeyVault(new Uri($"https://{keyVaultName}.vault.azure.net/keys/DataProtectionKey"), new DefaultAzureCredential());
-    }
+			.PersistKeysToAzureBlobStorage(blobClient)
+			.ProtectKeysWithAzureKeyVault(new Uri($"https://{keyVaultName}.vault.azure.net/keys/DataProtectionKey"), new DefaultAzureCredential());
+	}
 }
