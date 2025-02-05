7f277cdbead12052e741129fb47b9dba067a72de
Richard Edwards <richard.edwards@remarkable.global>
2025-02-05 19:35:07 +0000

    Add UmbrellaForwardedHeaderNames for Azure Front Door
    Introduce UmbrellaForwardedHeaderNames class with AzureFrontDoorForwardedHost constant for custom header in Azure Front Door. Add XML docs for clarity.

---
+++ Core/src/Umbrella.WebUtilities/Http/Constants/UmbrellaForwardedHeaderNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Umbrella.WebUtilities.Http.Constants;
+
+/// <summary>
+/// Contains the names of the forwarded headers that are used by Umbrella.
+/// </summary>
+public static class UmbrellaForwardedHeaderNames
+{
+	/// <summary>
+	/// The name of the header that contains the original host that the request was made to
+	/// when the request has been forwarded by Azure Front Door.
+	/// </summary>
+	/// <remarks>
+	/// This is a custom header which must be manually added to the request by Azure Front Door
+	/// using the Rule Sets feature. It is not added automatically by Azure Front Door.
+	/// </remarks>
+	public const string AzureFrontDoorForwardedHost = "X-AFD-Fowarded-Host";
+}
