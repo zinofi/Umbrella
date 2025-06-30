81b877c9a2f96fad59a944cf8718e65367d70d98
Richard Edwards <richard.edwards@ultimedia.agency>
2025-06-30 17:33:48 +0100

    Trimmable String Generator (WIP)

---
+++ Core/src/Umbrella.Utilities/Text/IUmbrellaTrimmable.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbrella.Utilities.Text;
+
+/// <summary>
+/// Interface for types that support automatic trimming of string properties.
+/// </summary>
+public interface IUmbrellaTrimmable
+{
+	/// <summary>
+	/// Trims all string properties in this object and its nested properties.
+	/// </summary>
+	void TrimAllStringProperties();
+}
