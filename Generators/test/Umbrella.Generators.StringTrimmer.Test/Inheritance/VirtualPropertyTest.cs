3794d9d08c931acb996fe32fd349927a521ff308
Richard Edwards <richard.edwards@remarkable.global>
2025-07-05 19:47:40 +0100

    Updates to allow types from the Microsoft.AspNetCore.Identity namespace to be trimmable.

---
+++ Generators/test/Umbrella.Generators.StringTrimmer.Test/Inheritance/VirtualPropertyTest.cs
@@ -0,0 +1,27 @@
+﻿using Umbrella.Utilities.Text;
+
+namespace Umbrella.Generators.StringTrimmer.Test.Inheritance;
+
+// Base class with virtual string properties
+public abstract class BaseClassWithVirtual
+{
+    public virtual string VirtualBaseStringProperty { get; set; } = "";
+    public virtual string? VirtualBaseNullableStringProperty { get; set; }
+    public string NonVirtualBaseStringProperty { get; set; } = "";
+}
+
+// Middle class overriding some properties
+public class MiddleClassWithOverrides : BaseClassWithVirtual
+{
+    public override string VirtualBaseStringProperty { get; set; } = "";
+    public string MiddleStringProperty { get; set; } = "";
+    public virtual string? VirtualMiddleStringProperty { get; set; }
+}
+
+// Derived class implementing IUmbrellaTrimmable - should trim all inherited properties
+// including virtual and overridden ones
+public partial class DerivedClassWithVirtual : MiddleClassWithOverrides, IUmbrellaTrimmable
+{
+    public override string? VirtualMiddleStringProperty { get; set; }
+    public string DerivedStringProperty { get; set; } = "";
+}
---
+++ Generators/test/Umbrella.Generators.StringTrimmer.Test/Tests/VirtualPropertyTests.cs
@@ -0,0 +1,42 @@
+﻿using Umbrella.Generators.StringTrimmer.Test.Inheritance;
+
+namespace Umbrella.Generators.StringTrimmer.Test.Tests;
+
+public class VirtualPropertyTests
+{
+    [Fact]
+    public void DerivedClassWithVirtual_TrimAllStringProperties_TrimsAllInheritedVirtualProperties()
+    {
+        // Arrange
+        var derivedClass = new DerivedClassWithVirtual
+        {
+            // Base class virtual properties
+            VirtualBaseStringProperty = "  VirtualBaseValue  ",
+            VirtualBaseNullableStringProperty = "  VirtualBaseNullableValue  ",
+            NonVirtualBaseStringProperty = "  NonVirtualBaseValue  ",
+            
+            // Middle class properties
+            MiddleStringProperty = "  MiddleValue  ",
+            VirtualMiddleStringProperty = "  VirtualMiddleValue  ",
+            
+            // Derived class properties
+            DerivedStringProperty = "  DerivedValue  "
+        };
+
+        // Act
+        derivedClass.TrimAllStringProperties();
+
+        // Assert
+        // Base class properties
+        Assert.Equal("VirtualBaseValue", derivedClass.VirtualBaseStringProperty);
+        Assert.Equal("VirtualBaseNullableValue", derivedClass.VirtualBaseNullableStringProperty);
+        Assert.Equal("NonVirtualBaseValue", derivedClass.NonVirtualBaseStringProperty);
+        
+        // Middle class properties
+        Assert.Equal("MiddleValue", derivedClass.MiddleStringProperty);
+        Assert.Equal("VirtualMiddleValue", derivedClass.VirtualMiddleStringProperty);
+        
+        // Derived class properties
+        Assert.Equal("DerivedValue", derivedClass.DerivedStringProperty);
+    }
+}
