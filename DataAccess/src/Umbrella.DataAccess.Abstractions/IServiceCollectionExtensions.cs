c5a1c4086e91880fd3ed7930096d86446380e1fc
Richard Edwards <richard.edwards@remarkable.global>
2025-11-17 18:08:40 +0000

    Refactor AddUmbrellaDataAccess method signature
    Updated the `AddUmbrellaDataAccess` method in `IServiceCollectionExtensions` to accept an optional `repoDataServiceOptionsBuilder` parameter. Refactored the method structure, including moving it inside a nested definition and updating XML documentation. Added a call to `ConfigureUmbrellaOptions` for configuring options. Reformatted service registrations for better alignment while retaining their functionality.

--- DataAccess/src/Umbrella.DataAccess.Abstractions/IServiceCollectionExtensions.cs
+++ DataAccess/src/Umbrella.DataAccess.Abstractions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 
 using CommunityToolkit.Diagnostics;
 using Umbrella.DataAccess.Abstractions;
+using Umbrella.DataAccess.Abstractions.Options;
 
 #pragma warning disable IDE0130
 namespace Microsoft.Extensions.DependencyInjection;
@@ -13,23 +14,30 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class IServiceCollectionExtensions
 {
-	/// <summary>
-	/// Adds the <see cref="Umbrella.DataAccess.Abstractions"/> services to the specified <see cref="IServiceCollection"/> dependency injection container builder.
-	/// </summary>
 	/// <param name="services">The services dependency injection container builder to which the services will be added.</param>
-	/// <returns>The <see cref="IServiceCollection"/> dependency injection container builder.</returns>
 	/// <exception cref="ArgumentNullException">Thrown if the <paramref name="services"/> is null.</exception>
-	public static IServiceCollection AddUmbrellaDataAccess(this IServiceCollection services)
+	extension(IServiceCollection services)
 	{
-		Guard.IsNotNull(services);
+		/// <summary>
+		/// Adds the <see cref="Umbrella.DataAccess.Abstractions"/> services to the specified <see cref="IServiceCollection"/> dependency injection container builder.
+		/// </summary>
+		/// <param name="repoDataServiceOptionsBuilder"></param>
+		/// <returns>The <see cref="IServiceCollection"/> dependency injection container builder.</returns>
+		public IServiceCollection AddUmbrellaDataAccess(
+			Action<IServiceProvider, UmbrellaRepositoryDataServiceOptions>? repoDataServiceOptionsBuilder = null)
+		{
+			Guard.IsNotNull(services);
 
-		_ = services.AddSingleton<IEntityValidator, EntityValidator>();
-		_ = services.AddSingleton<IEntityMappingUtility, EntityMappingUtility>();
-		_ = services.AddScoped<IUmbrellaDbContextHelper, UmbrellaDbContextHelper>();
-		_ = services.AddScoped(typeof(DbAppTenantSessionContext<>));
+			_ = services.AddSingleton<IEntityValidator, EntityValidator>();
+			_ = services.AddSingleton<IEntityMappingUtility, EntityMappingUtility>();
+			_ = services.AddScoped<IUmbrellaDbContextHelper, UmbrellaDbContextHelper>();
+			_ = services.AddScoped(typeof(DbAppTenantSessionContext<>));
 
-		_ = services.AddScoped<IUmbrellaRepositoryCoreDataService, UmbrellaRepositoryCoreDataService>();
+			_ = services.AddScoped<IUmbrellaRepositoryCoreDataService, UmbrellaRepositoryCoreDataService>();
 
-		return services;
+			_ = services.ConfigureUmbrellaOptions(repoDataServiceOptionsBuilder);
+
+			return services;
+		}
 	}
 }
